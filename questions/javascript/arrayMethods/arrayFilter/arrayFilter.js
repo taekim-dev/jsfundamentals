99a8f93729c195ba6549b896e97d2766426df027
Tae Kim <taekim.dev@gmail.com>
2024-04-29 17:01:56 +0800

    add skeleton for arrayFilter

---
+++ questions/javascript/arrayMethods/arrayFilter/arrayFilter.js
@@ -0,0 +1,12 @@
+
+function myFilter(array, callback, thisArg) {
+
+
+
+}
+
+module.export = myFilter;
+
+const numbers = [1, 2, 3, 4, 5, 6];
+const evenNumbers = numbers.myFilter(item => item % 2 === 0);
+console.log(evenNumbers);
