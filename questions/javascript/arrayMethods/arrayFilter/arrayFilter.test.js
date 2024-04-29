35fcc0c056f845f7b17e07730fd4d36e274b9b30
Tae Kim <taekim.dev@gmail.com>
2024-04-29 17:24:58 +0800

    add test case using a thisArg

--- questions/javascript/arrayMethods/arrayFilter/arrayFilter.test.js
+++ questions/javascript/arrayMethods/arrayFilter/arrayFilter.test.js
@@ -6,14 +6,17 @@ describe('arrayFilter', () => {
             const input = [1, 2, 3, 4, 5, 6];
             const result = arrayFilter(input, x => x % 2 === 1);
             expect(result).toEqual([1, 3, 5]);
-        })
+        });
 
-        // test('applies a function with this argument', () => {
-        //     const input = [1, 2, 3];
-        //     const initialValue = 2;
-        //     const result = arrayFilter(input, (acc, curr) => acc + curr, initialValue);
-        //     expect(result).toEqual([3, 5, 8]);
-        // })
+        test('applies a function to every element using thisArg to provide context', () => {
+            const input = [1, 2, 3, 4, 5, 6];
+            const thresholds = { minValue : 2}
+            const result = arrayFilter(input, function(x) {
+                return (x % 2 === 1 && x > this.minValue)
+            }, thresholds)
+            expect(result).toEqual([3, 5])
+        });
+        
     })
 
     describe('edge cases', () => {
@@ -21,6 +24,6 @@ describe('arrayFilter', () => {
             const input = [];
             const result = arrayFilter(input, x => x % 2 === 0);
             expect(result).toEqual([]);
-        })
+        });
     })
 })
