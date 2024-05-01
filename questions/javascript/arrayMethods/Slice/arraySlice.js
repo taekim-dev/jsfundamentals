541a09e09bcc997760612e7823e0e6dc12268af5
Tae Kim <taekim.dev@gmail.com>
2024-05-01 21:40:07 +0800

    refactor slice implementation

--- questions/javascript/arrayMethods/Slice/arraySlice.js
+++ questions/javascript/arrayMethods/Slice/arraySlice.js
@@ -2,18 +2,18 @@
 Array.prototype.arraySlice = function(startIndex, endIndex) {
     if (this.length === 0) return [];
     const res = [];
-    if (startIndex !== undefined && endIndex === undefined){
-        if (startIndex >= 0){
-            for(let i = startIndex; i < this.length; i++){
-                res.push(this[i]);
-            }
-        } else {
-            startIndex = startIndex + this.length;
-            for(let i = startIndex; i < this.length; i++ ){
-                res.push(this[i]);
-            }
-        }
+    
+    startIndex = startIndex < 0 ? this.length + startIndex : startIndex;
+    if (endIndex === undefined) {
+        endIndex = this.length;
+    } else {
+        endIndex = endIndex < 0 ? this.length + endIndex : endIndex;
     }
+
+    for(let i = startIndex; i < endIndex; i++){
+        res.push(this[i]);
+    }
+
     return res;
 
     // for(let i = startIndex; i < endIndex; i++) {
