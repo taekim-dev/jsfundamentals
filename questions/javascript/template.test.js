ddff6360e4b67c45ebbacf2fae538019fae360ab
Tae Kim <taekim.dev@gmail.com>
2024-05-01 14:44:24 +0800

    add template js and test

---
+++ questions/javascript/template.test.js
@@ -0,0 +1,13 @@
+const methodName = require('./template.js')
+
+describe('Function Name', () => {
+    describe('standard behavior', () => {
+        test('description', () => {
+        });
+    })
+
+    describe('edge cases', () => {
+        test('description', () => {
+        });
+    })
+})
