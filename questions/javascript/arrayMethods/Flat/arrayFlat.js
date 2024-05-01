13161239f1011fab78422f41d21ecf87cc7b33f6
Tae Kim <taekim.dev@gmail.com>
2024-05-01 15:04:04 +0800

    add a basic flat implementation

--- questions/javascript/arrayMethods/Flat/arrayFlat.js
+++ questions/javascript/arrayMethods/Flat/arrayFlat.js
@@ -1,9 +1,16 @@
-function arrayFlat() {
-
+Array.prototype.arrayFlat = function(depth) {
+    if (this.length === 0) return [];
+    let res = [];
+    if (depth === undefined || depth === 1) {
+        for(const obj of this){
+            if (!Array.isArray(obj)){
+                res.push(obj);
+            } else {
+                for (const element of obj){
+                    res.push(element);
+                }
+            }
+        }
+    }
+    return res;
 }
-
-Array.prototype.arrayFlat = function() {
-    
-}
-
-module.exports = arrayFlat;
