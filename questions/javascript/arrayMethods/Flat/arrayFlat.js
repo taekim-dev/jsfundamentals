c045bcec66980eb80aeb5033b47fc311a3cd4ad5
Tae Kim <taekim.dev@gmail.com>
2024-05-01 14:43:07 +0800

    add skeleton for ArrayFlat

---
+++ questions/javascript/arrayMethods/Flat/arrayFlat.js
@@ -0,0 +1,5 @@
+function arrayFlat() {
+
+}
+
+module.exports = arrayFlat;
---
+++ questions/javascript/arrayMethods/Flat/arrayFlat.test.js
@@ -0,0 +1,13 @@
+const arrayFlat = require('./arrayFlat.js')
+
+describe('Function Name', () => {
+    describe('standard behavior', () => {
+        test('description', () => {
+        });
+    })
+
+    describe('edge cases', () => {
+        test('description', () => {
+        });
+    })
+})
