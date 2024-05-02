045a7119308a71c9c45201aae63fe3fc615cd2b9
Taewan Kim <taekim.dev@gmail.com>
2024-05-02 21:54:51 +0800

    add basic test case

--- questions/javascript/arrayMethods/Splice/mySplice.test.js
+++ questions/javascript/arrayMethods/Splice/mySplice.test.js
@@ -2,8 +2,29 @@ require('./mySplice.js')
 
 describe('Custom Splice', () => {
     describe('standard behavior', () => {
-        test('description', () => {
-            expect().toEqual();
+        test('handles when no argument is given', () => {
+            const array = [1, 2, 3];
+            const res = array.mySplice;
+            expect(res).toEqual([]);
+            expect(array).toEqual([1, 2, 3])
+        });
+
+        test('handles when positive start index is given', () => {
+            const array = [1, 2, 3];
+            const res = array.mySplice(2, 1);
+            expect(res).toEqual([1, 2]);
+        });
+
+        test('handles when start index is nagative', () => {
+            const array = [1, 2, 3, 4];
+            const res = array.mySplice(-2, 2);
+            expect(res).toEqual([3, 4]);
+        });
+
+        test('handles when no delete count is given', () => {
+            const array = [1, 2, 4];
+            const res = array.mySplice(2, 0, 3);
+            expect(res).toEqual([1, 2, 3]);
         });
     })
 
