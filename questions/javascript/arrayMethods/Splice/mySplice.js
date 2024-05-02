b75d27aeeb771c6cfad6a73b076dc5a7da5414a6
Taewan Kim <taekim.dev@gmail.com>
2024-05-03 07:38:22 +0800

    add splice implementation

--- questions/javascript/arrayMethods/Splice/mySplice.js
+++ questions/javascript/arrayMethods/Splice/mySplice.js
@@ -1,5 +1,23 @@
-Function(Array).prototype.mySplice = function() {
+Array.prototype.mySplice = function(startIndex, removeCount, ...items) {
+    let removed = [];
+    startIndex = startIndex ?? 0;
+    startIndex = startIndex < 0 ? this.length + startIndex : startIndex;
+    startIndex = Math.max(0, Math.min(startIndex, this.length));  // Clamp startIndex
+    removeCount = removeCount !== undefined ? removeCount : this.length - startIndex;
 
+    if (removeCount > 0) {
+        removed = this.slice(startIndex, startIndex + removeCount);
+        this.copyWithin(startIndex, startIndex + removeCount);
+        this.length -= removeCount;  // Adjust length after copying
+    }
+
+    if (items.length > 0) {
+        this.length += items.length;
+        this.copyWithin(startIndex + items.length, startIndex, this.length - items.length - removeCount);
+        for (let i = 0; i < items.length; i++) {
+            this[startIndex + i] = items[i];
+        }
+    }
+
+    return removed;
 }
-
-
