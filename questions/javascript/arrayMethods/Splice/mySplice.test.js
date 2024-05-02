b9655740246839ba121b520665aab3e383e0ad1a
Taewan Kim <taekim.dev@gmail.com>
2024-05-02 22:36:52 +0800

    adjust the test cases

--- questions/javascript/arrayMethods/Splice/mySplice.test.js
+++ questions/javascript/arrayMethods/Splice/mySplice.test.js
@@ -11,28 +11,28 @@ describe('Custom Splice', () => {
 
         test('handles when positive start index is given', () => {
             const array = [1, 2, 3];
-            res = array.mySplice(2, 1);
+            const res = array.mySplice(2, 1);
             expect(res).toEqual([3]);
             expect(array).toEqual([1, 2]);
         });
 
         test('handles when start index is nagative', () => {
             const array = [1, 2, 3, 4];
-            res = array.mySplice(-2, 2);
+            const res = array.mySplice(-2, 2);
             expect(res).toEqual([3, 4]);
             expect(array).toEqual([1, 2]);
         });
 
         test('handles when no delete count is given', () => {
             const array = [1, 2, 4];
-            res = array.mySplice(2, 0, 3);
+            const res = array.mySplice(2, 0, 3);
             expect(res).toEqual([]);
             expect(array).toEqual([1, 2, 3, 4]);
         });
     })
 
-    describe('edge cases', () => {
-        test('description', () => {
-        });
-    })
+    // describe('edge cases', () => {
+    //     test('description', () => {
+    //     });
+    // })
 })
