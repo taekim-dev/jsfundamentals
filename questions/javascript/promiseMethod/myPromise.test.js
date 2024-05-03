6ddb7d494262ea33d35d579cc7f91177fae3774c
Tae Kim <taekim.dev@gmail.com>
2024-05-03 22:10:14 +0800

    add test cases for the promise

--- questions/javascript/promiseMethod/myPromise.test.js
+++ questions/javascript/promiseMethod/myPromise.test.js
@@ -2,7 +2,37 @@ require('./myPromise.js')
 
 describe('Custom Promise', () => {
     describe('standard behavior', () => {
-        test('description', () => {
+        test('resolve a valid function', async () => {
+            const log = () => "hello";
+            const res = await new MyPromise((resolve) => {
+                resolve(log());
+            });
+
+            expect(res).toEqual("hello");
+        });
+
+        test('resolve a valid function with timeout', async () => {
+            const log = () => {
+                return new myPromise((resolve) => {
+                    setTimeout(() => {
+                        resolve("hello");
+                    }, 1000)
+                })
+            }
+
+            const res = await log();
+            expect(res).toEqual("hello");
+        });
+
+        test('reject a valid function', () => {
+            expect().toEqual();
+        });
+
+        test('handles chaining one statement', () => {
+            expect().toEqual();
+        });
+
+        test('handles chaining multiple statements', () => {
             expect().toEqual();
         });
     })
