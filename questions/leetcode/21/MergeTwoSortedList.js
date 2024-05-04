b24db898bbb0f90a697e33b13d201128fb409783
Tae Kim <taekim.dev@gmail.com>
2024-05-04 18:47:08 +0800

    add recursive solution

--- questions/leetcode/21/MergeTwoSortedList.js
+++ questions/leetcode/21/MergeTwoSortedList.js
@@ -1,27 +1,38 @@
+// Iterative solution
+// function mergeList(list1, list2) {
+//     let dummyHead = new ListNode(0);
+//     let current = dummyHead;
+
+//     while (list1 !== null && list2 !== null) {
+//         if (list1.val < list2.val) {
+//             current.next = list1;
+//             list1 = list1.next;
+
+//         } else {
+//             current.next = list2;
+//             list2 = list2.next;
+//         }
+//         current = current.next;
+//     }
+
+//     current.next = list1 !== null ? list1 : list2;
+
+//     return dummyHead.next;
+
+// }
+
+// Recursive solution
 function mergeList(list1, list2) {
-    if (list1 === null || list2 === null){
-        return list1 || list2;
+    if (list1 === null) return list2
+    if (list2 === null) return list1
+
+    if (list1.val < list2.val) {
+        list1.next = mergeList(list1.next, list2);
+        return list1;
+    } else {
+        list2.next = mergeList(list1, list2.next)
+        return list2;
     }
-
-    let dummyHead = new ListNode(0);
-    let currMerged = dummyHead;
-
-    while (list1 !== null && list2 !== null) {
-        if (list1.val < list2.val) {
-            currMerged.next = list1;
-            list1 = list1.next;
-
-        } else {
-            currMerged.next = list2;
-            list2 = list2.next;
-        }
-        currMerged = currMerged.next;
-    }
-
-    currMerged.next = list1 || list2;
-
-    return dummyHead.next;
-
 }
 
 function ListNode(val, next) {
