c17908be668a02b4c7fa4f732f6ba788c32f94d2
Tae Kim <taekim.dev@gmail.com>
2024-05-04 18:40:40 +0800

    implement mergeTwoSortedList

--- questions/leetcode/21/MergeTwoSortedList.js
+++ questions/leetcode/21/MergeTwoSortedList.js
@@ -1,4 +1,26 @@
-function mergeList() {
+function mergeList(list1, list2) {
+    if (list1 === null || list2 === null){
+        return list1 || list2;
+    }
+
+    let dummyHead = new ListNode(0);
+    let currMerged = dummyHead;
+
+    while (list1 !== null && list2 !== null) {
+        if (list1.val < list2.val) {
+            currMerged.next = list1;
+            list1 = list1.next;
+
+        } else {
+            currMerged.next = list2;
+            list2 = list2.next;
+        }
+        currMerged = currMerged.next;
+    }
+
+    currMerged.next = list1 || list2;
+
+    return dummyHead.next;
 
 }
 
