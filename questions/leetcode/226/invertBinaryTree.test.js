220f6768e69329f0a60f861a745c3c1db0812f0c
Taewan Kim <taekim.dev@gmail.com>
2024-05-05 09:33:35 +0800

    add invert binary tree question

---
+++ questions/leetcode/226/invertBinaryTree.test.js
@@ -0,0 +1,21 @@
+const invertTree = require('./invertBinaryTree.js')
+
+describe('invertTree', () => {
+    describe('standard behavior', () => {
+        test('', () => {
+
+            expect(result).toEqual(false);
+        });
+
+        test('', () => {
+            expect(result).toEqual(true);
+        });
+        
+    })
+
+    describe('edge case', () => {
+        test('', () => {
+        });
+
+    })
+})
