7f1ec07de24ce04fbefe31f79bd9cbdf18fa555f
Taewan Kim <taekim.dev@gmail.com>
2024-05-06 07:55:55 +0800

    adding skeleton for custom currying

---
+++ questions/javascript/utilityMethods/curry.js
@@ -0,0 +1,4 @@
+Function(Array).prototype.curry = function() {
+
+}
+
---
+++ questions/javascript/utilityMethods/curry.test.js
@@ -0,0 +1,16 @@
+require('./methodName.js')
+
+describe('Test Curry Function', () => {
+    describe('standard behavior', () => {
+        test('handles a simple calculation', () => {
+
+            expect().toEqual();
+        });
+    })
+
+    describe('edge cases', () => {
+        test('description', () => {
+
+        });
+    })
+})
