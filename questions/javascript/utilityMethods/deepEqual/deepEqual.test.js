a0a900baea62b6685cf334f8537b7407a104d1b5
Taewan Kim <taekim.dev@gmail.com>
2024-05-07 07:57:12 +0800

    add more test cases

--- questions/javascript/utilityMethods/deepEqual/deepEqual.test.js
+++ questions/javascript/utilityMethods/deepEqual/deepEqual.test.js
@@ -12,6 +12,22 @@ describe('Deep Equal', () => {
             expect(res).toEqual(true);
         });
 
+        test('handles undefined values', () => {
+            const res = deepEqual(undefined, undefined);
+            expect(res).toEqual(true);
+        });
+
+        test('differentiates null from undefined', () => {
+            const res = deepEqual(null, undefined);
+            expect(res).toEqual(false);
+        });        
+
+        test('handles Date objects', () => {
+            const date1 = new Date(2020, 1, 1);
+            const date2 = new Date(2020, 1, 1);
+            const res = deepEqual(date1, date2);
+            expect(res).toEqual(true);
+        });
 
         test('handles two bigInt equal', () => {
             const bigNumber1 = 19393939393n;
@@ -42,6 +58,17 @@ describe('Deep Equal', () => {
     })
 
     describe('edge cases', () => {
+        test('calls the function in reversed order', () => {
+            const arrayOne = [1, 2, 3];
+            const arrayTwo = [1, 2, 3];
+
+            const resOne = deepEqual(arrayOne, arrayTwo);
+            expect(resOne).toEqual(true);
+
+            const resTwo = deepEqual(arrayTwo, arrayOne);
+            expect(resTwo).toEqual(true);
+        });
+
         test('handles two different types unequal', () => {
             const res = deepEqual(1, "1")
             expect(res).toEqual(false);
@@ -74,5 +101,32 @@ describe('Deep Equal', () => {
             const res = deepEqual(obj1, obj2)
             expect(res).toEqual(true);
         });
+
+        test('handles objects with properties in different orders', () => {
+            const obj1 = { a: 1, b: 2 };
+            const obj2 = { b: 2, a: 1 };
+            const res = deepEqual(obj1, obj2);
+            expect(res).toEqual(true);
+        });
+        
+        test('handles recursive objects', () => {
+            const obj1 = {};
+            obj1.self = obj1;
+            const obj2 = {};
+            obj2.self = obj2;
+            const res = deepEqual(obj1, obj2);
+            expect(res).toEqual(true);
+        });
+
+        test('handles objects with null and undefined properties', () => {
+            const obj1 = { a: null, b: 4 };
+            const obj2 = { a: undefined, b: 4 };
+            const obj3 = { b: 4 };
+        
+            expect(deepEqual(obj1, obj2)).toEqual(false);
+            expect(deepEqual(obj1, obj3)).toEqual(false);
+            expect(deepEqual(obj2, obj3)).toEqual(false);
+        });
+        
     })
 })
