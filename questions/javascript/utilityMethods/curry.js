21de06e495d97b8320ec49c626ef421929d362f7
Taewan Kim <taekim.dev@gmail.com>
2024-05-06 08:16:50 +0800

    add basic curry implementation

--- questions/javascript/utilityMethods/curry.js
+++ questions/javascript/utilityMethods/curry.js
@@ -1,4 +1,12 @@
-Function(Array).prototype.curry = function() {
-
+Function.prototype.curry = function() {
+    const fn = this;
+    return function curried(...args) {
+        if (args.length >= fn.length) {
+            return fn.apply(this, args);
+        } else {
+            return function(...args2) {
+                return curried.apply(this, args.concat(args2));
+            }
+        }
+    }
 }
-
