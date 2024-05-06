f5b0a090bc92ab20881fb70137c91679ebb6f3f9
Taewan Kim <taekim.dev@gmail.com>
2024-05-06 08:01:13 +0800

    add test cases for currying

--- questions/javascript/utilityMethods/curry.test.js
+++ questions/javascript/utilityMethods/curry.test.js
@@ -3,8 +3,26 @@ require('./methodName.js')
 describe('Test Curry Function', () => {
     describe('standard behavior', () => {
         test('handles a simple calculation', () => {
+            const sum = (a, b) => a + b;
+            const curriedSum = curry(sum);
+            const addTwo = curriedSum(2);
+            const res = addTwo(3);
 
-            expect().toEqual();
+            expect(res).toEqual(5);
+        });
+
+        test('handles a multiple calculations', () => {
+            const multiply = (a, b) => a * b;
+            const curriedSum = curry(multiply);
+            const multiplyTwo = curriedSum(2);
+            const multiplyThree = curriedSum(3);
+
+            const resTwo = multiplyTwo(2);
+            const resThree = multiplyThree(2);
+
+            expect(resTwo).toEqual(4);
+            expect(resThree).toEqual(6);
+
         });
     })
 
