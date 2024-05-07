f9b01823e3e2037b15f50af729b84d3f56ba4ce4
Taewan Kim <taekim.dev@gmail.com>
2024-05-07 20:50:02 +0800

    implement deep copy

--- questions/javascript/utilityMethods/deepCopy/deepCopy.js
+++ questions/javascript/utilityMethods/deepCopy/deepCopy.js
@@ -1,6 +1,26 @@
 function deepCopy(original) {
+    if (original === null) return null
+    if (original === undefined) return undefined
+
     if (typeof original !== 'object') return original;
-    
+
+    if (Array.isArray(original)) {
+        const copiedArray = [];
+        for (const element of original) {
+            const copiedElement = deepCopy(element)
+            copiedArray.push(copiedElement);
+        }
+        return copiedArray;
+    }
+
+    if (typeof original === 'object') {
+        const copied = {};
+        const keys = Object.keys(original);
+        for(const key of keys) {
+            copied[key] = deepCopy(original[key]);
+        }
+        return copied;
+    }
 
 }
 module.exports = deepCopy;
