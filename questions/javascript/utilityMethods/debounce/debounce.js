e244a0d90c08fac558a1b52360fa9e02c9bf46bd
Taewan Kim <taekim.dev@gmail.com>
2024-05-08 08:51:17 +0800

    add a basic debounce

--- questions/javascript/utilityMethods/debounce/debounce.js
+++ questions/javascript/utilityMethods/debounce/debounce.js
@@ -1,5 +1,11 @@
-function debounce() {
-
+function debounce(func, delay) {
+    let timerId;
+    return function (...args) {
+        clearTimeout(timerId);
+        timerId = setTimeout(() => {
+            func.apply(this, args)
+        }, delay)
+    }
 }
 
 module.exports = debounce;
