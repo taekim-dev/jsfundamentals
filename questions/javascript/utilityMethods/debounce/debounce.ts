94a8a06ddf2c4b32d6864cc954593295624b0ec7
Taewan Kim <taekim.dev@gmail.com>
2024-05-08 21:34:32 +0800

    add typescript version

---
+++ questions/javascript/utilityMethods/debounce/debounce.ts
@@ -0,0 +1,13 @@
+function debounce<F extends (...args: any[]) => void>(func: F, delay: number): (...args: Parameters<F>) => void {
+    let timer : NodeJS.Timeout;
+
+    return (...args: Parameters<F>) => {
+        clearTimeout(timer);
+
+        timer = setTimeout(() => {
+            func(...args);
+        }, delay)
+    }
+}
+
+
