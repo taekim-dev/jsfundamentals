4597c18240a6d85712301239f88da078737f513d
Taewan Kim <taekim.dev@gmail.com>
2024-05-08 08:44:28 +0800

    add debounce tests

--- questions/javascript/utilityMethods/debounce/debounce.test.js
+++ questions/javascript/utilityMethods/debounce/debounce.test.js
@@ -1,9 +1,51 @@
 const debounce = require('./debounce.js')
 
-describe('Debounce', () => {
+describe('Debounce Funtionality', () => {
     describe('standard behavior', () => {
-        test('description', () => {
-            expect().toEqual();
+        test('handles no delay', done => {
+            jest.useFakeTimers();
+            const mockFunction = jest.fn();
+
+            const debounced = debounce(mockFunction, 0);
+            debounced();
+            debounced();
+
+            jest.runAllTimers();
+
+            expect(mockFunction).toHaveBeenCalledTimes(1);
+            done();
+        });
+
+        test('handles a short delay', done => {
+            jest.useFakeTimers();
+            const mockFunction = jest.fn();
+
+            const debounced = debounce(mockFunction, 1);
+            debounced();
+            debounced();
+            debounced();
+            debounced();
+
+            jest.runAllTimers();
+
+            expect(mockFunction).toHaveBeenCalledTimes(1);
+            done();
+        });
+
+        test('only the last of three calls is executed after 1000ms delay', () => {
+            jest.useFakeTimers();
+            const mockFunction = jest.fn();
+            const debouncedFunction = debounce(mockFunction, 1000);
+    
+            debouncedFunction();
+            debouncedFunction();
+            debouncedFunction();
+    
+            // Fast-forward time by 1000ms
+            jest.advanceTimersByTime(1000);
+    
+            expect(mockFunction).toHaveBeenCalledTimes(1);
+            jest.clearAllTimers();
         });
     })
 
