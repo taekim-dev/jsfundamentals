e54cd00ba37e5a49497eda1b0ec4e3873c475730
Taewan Kim <taekim.dev@gmail.com>
2024-05-09 08:30:17 +0800

    add throttle skeleton

---
+++ questions/javascript/utilityMethods/throttle/throttle.js
@@ -0,0 +1,6 @@
+function throttle() {
+
+}
+
+module.exports = throttle;
+
---
+++ questions/javascript/utilityMethods/throttle/throttle.test.js
@@ -0,0 +1,14 @@
+const throttle = require('./throttle.js')
+
+describe('Throttle Test', () => {
+    describe('standard behavior', () => {
+        test('description', () => {
+            expect().toEqual();
+        });
+    })
+
+    describe('edge cases', () => {
+        test('description', () => {
+        });
+    })
+})
