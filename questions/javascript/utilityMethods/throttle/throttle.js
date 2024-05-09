54468807b1e07e0f8b5c4bd6cc68f9c3acefb7cb
Taewan Kim <taekim.dev@gmail.com>
2024-05-09 08:44:00 +0800

    add basic throttle implementation with tests

--- questions/javascript/utilityMethods/throttle/throttle.js
+++ questions/javascript/utilityMethods/throttle/throttle.js
@@ -1,5 +1,14 @@
-function throttle() {
-
+function throttle(Func, delay) {
+    let timerFlag = null
+    
+    return function(...args) {
+        if (timerFlag === null) {
+            Func.apply(this, args);
+            timerFlag = setTimeout(() => {
+                timerFlag = null
+            }, delay)
+        }
+    }
 }
 
 module.exports = throttle;
--- questions/javascript/utilityMethods/throttle/throttle.test.js
+++ questions/javascript/utilityMethods/throttle/throttle.test.js
@@ -2,8 +2,31 @@ const throttle = require('./throttle.js')
 
 describe('Throttle Test', () => {
     describe('standard behavior', () => {
-        test('description', () => {
-            expect().toEqual();
+        test('Allows a call after the delay', () => {
+            jest.useFakeTimers();
+            const mockFunction = jest.fn();
+        
+            const throttled = throttle(mockFunction, 2);
+            throttled();
+            jest.advanceTimersByTime(3);
+            throttled();
+        
+            expect(mockFunction).toHaveBeenCalledTimes(2);
+        });
+
+        test('Allows a single call during the delay', () => {
+            jest.useFakeTimers();
+            const mockFunction = jest.fn();
+        
+            const throttled = throttle(mockFunction, 2);
+            throttled();
+            throttled();
+            throttled();
+            throttled();
+            jest.advanceTimersByTime(3);
+            throttled();
+        
+            expect(mockFunction).toHaveBeenCalledTimes(2);
         });
     })
 
