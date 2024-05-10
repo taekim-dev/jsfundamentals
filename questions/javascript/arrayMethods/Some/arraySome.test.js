26b8e0706c86fa048530946ef86c221679d4eb76
Taewan Kim <taekim.dev@gmail.com>
2024-05-10 08:40:56 +0800

    add skeleton for array Some method

---
+++ questions/javascript/arrayMethods/Some/arraySome.test.js
@@ -0,0 +1,14 @@
+require('./arraySome.js')
+
+describe('Array Some method', () => {
+    describe('standard behavior', () => {
+        test('description', () => {
+            expect().toEqual();
+        });
+    })
+
+    describe('edge cases', () => {
+        test('description', () => {
+        });
+    })
+})
