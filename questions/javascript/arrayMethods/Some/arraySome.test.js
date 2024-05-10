4a107ba9d000462637042c6f048e01f78a08fc9e
Taewan Kim <taekim.dev@gmail.com>
2024-05-10 21:55:35 +0800

    fix the test case

--- questions/javascript/arrayMethods/Some/arraySome.test.js
+++ questions/javascript/arrayMethods/Some/arraySome.test.js
@@ -2,7 +2,7 @@ require('./arraySome.js')
 
 describe('Array Some method', () => {
     describe('standard behavior', () => {
-        test('applied with a simple callback function', () => {
+        test('applied with a simple callback function with a true condition', () => {
             const input = [1, 2, 3];
             const even = (element) => element % 2 === 0;
             const res = input.arraySome(even)
@@ -10,31 +10,38 @@ describe('Array Some method', () => {
             expect(res).toEqual(true);
         });
 
+        test('applied with a simple callback function with a false condition', () => {
+            const input = [1, 3, 5, 7];
+            const even = (element) => element % 2 === 0;
+            const res = input.arraySome(even)
+
+            expect(res).toEqual(false);
+        });
+
         test('applied with a complex callback function', () => {
-            const input = {
-                name : "Kim",
-                address : {
-                    country : "Korea",
-                    city: "Seoul"
+            const input = [
+                {
+                    name: "Kim",
+                    address: {
+                        country: "Korea",
+                        city: "Seoul"
+                    }
                 },
-                
-                name : "David",
-                address : {
-                    country : "Canada",
-                    city: "Montreal"
-                }   
-            };
-            const isPersonUSbased = (peopleList) => {
-                for (person in peopleList) {
-                    if (person?.address?.city === "United States"){
-                        return true;
+                {
+                    name: "David",
+                    address: {
+                        country: "Canada",
+                        city: "Montreal"
                     }
                 }
-
-                return false
-            }
+            ];
+            const isPersonUSbased = (person) => {
+                    if (person.address && person.address.city === "United States") {
+                        return true;
+                }
+                return false;
+            };
             const res = input.arraySome(isPersonUSbased)
-
             expect(res).toEqual(false);
         });
 
