d6c8ef0f4debde64097ed0efbba565e39697f800
Taewan Kim <taekim.dev@gmail.com>
2024-05-10 08:51:12 +0800

    add basic test cases for array Some

--- questions/javascript/arrayMethods/Some/arraySome.test.js
+++ questions/javascript/arrayMethods/Some/arraySome.test.js
@@ -2,8 +2,48 @@ require('./arraySome.js')
 
 describe('Array Some method', () => {
     describe('standard behavior', () => {
-        test('description', () => {
-            expect().toEqual();
+        test('applied with a simple callback function', () => {
+            const input = [1, 2, 3];
+            const even = (element) => element % 2 === 0;
+            const res = input.arraySome(even)
+
+            expect(res).toEqual(true);
+        });
+
+        test('applied with a complex callback function', () => {
+            const input = {
+                name : "Kim",
+                address : {
+                    country : "Korea",
+                    city: "Seoul"
+                },
+                
+                name : "David",
+                address : {
+                    country : "Canada",
+                    city: "Montreal"
+                }   
+            };
+            const isPersonUSbased = (peopleList) => {
+                for (person in peopleList) {
+                    if (person?.address?.city === "United States"){
+                        return true;
+                    }
+                }
+
+                return false
+            }
+            const res = input.arraySome(isPersonUSbased)
+
+            expect(res).toEqual(false);
+        });
+
+        test('applied with a this argument', () => {
+            const input = [1, 2, 3];
+            const even = (element) => element % 2 === 0;
+            const res = input.arraySome(even)
+
+            expect(res).toEqual(true);
         });
     })
 
