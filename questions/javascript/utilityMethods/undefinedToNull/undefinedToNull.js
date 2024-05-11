6798cd298c64c424165a2cdf2d69d8c1830a5896
Taewan Kim <taekim.dev@gmail.com>
2024-05-11 09:52:03 +0800

    add skeleton for undefined to null

---
+++ questions/javascript/utilityMethods/undefinedToNull/undefinedToNull.js
@@ -0,0 +1,6 @@
+function undefinedToNull() {
+
+}
+
+module.exports = undefinedToNull;
+
---
+++ questions/javascript/utilityMethods/undefinedToNull/undefinedToNull.test.js
@@ -0,0 +1,14 @@
+const undefinedToNull = require('./undefinedToNull.js')
+
+describe('Undefined To Null Name', () => {
+    describe('standard behavior', () => {
+        test('description', () => {
+            expect().toEqual();
+        });
+    })
+
+    describe('edge cases', () => {
+        test('description', () => {
+        });
+    })
+})
