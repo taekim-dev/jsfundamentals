b5fd58130f4616fa50813aeba9896e3ba765aa1b
Taewan Kim <taekim.dev@gmail.com>
2024-05-11 09:55:00 +0800

    add base test cases

--- questions/javascript/utilityMethods/undefinedToNull/undefinedToNull.test.js
+++ questions/javascript/utilityMethods/undefinedToNull/undefinedToNull.test.js
@@ -2,8 +2,29 @@ const undefinedToNull = require('./undefinedToNull.js')
 
 describe('Undefined To Null Name', () => {
     describe('standard behavior', () => {
-        test('description', () => {
-            expect().toEqual();
+        test('handles primitive type', () => {
+            const input = undefined;
+            const output = undefinedToNull(input)
+
+            expect(output).toEqual(null);
+        });
+
+        test('handles array type', () => {
+            const input = [1, 2, undefined];
+            const output = undefinedToNull(input)
+
+            expect(output).toEqual([1, 2, null]);
+        });
+
+        test('handles object type', () => {
+            const input = {
+                name : "kim",
+                number: "9990",
+                gender: undefined
+            };
+            const output = undefinedToNull(input)
+
+            expect(output.gender).toEqual(null);
         });
     })
 
