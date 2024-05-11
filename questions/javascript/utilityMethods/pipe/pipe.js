a70caeb09613b87eb10ce37170f7c7ff963f9c89
Taewan Kim <taekim.dev@gmail.com>
2024-05-11 21:47:41 +0800

    add skeleton for pipe

---
+++ questions/javascript/utilityMethods/pipe/pipe.js
@@ -0,0 +1,6 @@
+function pipe() {
+
+}
+
+module.exports = pipe;
+
---
+++ questions/javascript/utilityMethods/pipe/pipe.test.js
@@ -0,0 +1,14 @@
+const pipe = require('./pipe.js')
+
+describe('Pipe Functions', () => {
+    describe('standard behavior', () => {
+        test('description', () => {
+            expect().toEqual();
+        });
+    })
+
+    describe('edge cases', () => {
+        test('description', () => {
+        });
+    })
+})
