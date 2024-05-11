5584887230e3d8fc64b96835c01f1cfdd164117b
Taewan Kim <taekim.dev@gmail.com>
2024-05-11 10:23:03 +0800

    implement undefined to null

--- questions/javascript/utilityMethods/undefinedToNull/undefinedToNull.js
+++ questions/javascript/utilityMethods/undefinedToNull/undefinedToNull.js
@@ -1,6 +1,21 @@
-function undefinedToNull() {
+function undefinedToNull(input) {
+    if (input === undefined) {
+        return null;
+    }
+    
+    if (Array.isArray(input)) {
+        return input.map(element => undefinedToNull(element));
+    }
 
+    if (input !== null && typeof input === 'object') {
+        Object.keys(input).forEach(key => {
+            input[key] = undefinedToNull(input[key]);
+        });
+    }
+
+    return input;
 }
+
 
 module.exports = undefinedToNull;
 
