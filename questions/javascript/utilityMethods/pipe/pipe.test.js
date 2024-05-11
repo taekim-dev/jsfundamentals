9b7698306584e01ddccef3c03998579c9ee4f1da
Taewan Kim <taekim.dev@gmail.com>
2024-05-11 22:22:27 +0800

    update pipe test cases

--- questions/javascript/utilityMethods/pipe/pipe.test.js
+++ questions/javascript/utilityMethods/pipe/pipe.test.js
@@ -1,14 +1,55 @@
 const pipe = require('./pipe.js')
 
 describe('Pipe Functions', () => {
+    let times, plus, subtrack, divide;
+
+    beforeEach(() => {
+        times = (y) => (x) => x * y;
+        plus = (y) => (x) => x + y;
+        subtrack = (y) => (x) => x - y;
+        divide = (y) => (x) => x / y;
+    });
+
     describe('standard behavior', () => {
-        test('description', () => {
-            expect().toEqual();
+        
+        test('handles a single function', () => {
+            const input = 1;
+            const process = pipe([
+                substrack(1)
+            ]) 
+            const res = process(input)
+            expect(res).toEqual(0);
+        });
+
+        test('handles pipe two functions', () => {
+            const input = 1;
+            const process = pipe([
+                times(4),
+                plus(4),
+            ]) 
+            const res = process(input)
+            expect(res).toEqual(2);
+        });
+
+        test('handles pipe multiple combinations', () => {
+            const input = 1;
+            const process = pipe([
+                times(4),
+                plus(4),
+                divide(2),
+                subtrack(1)
+            ]) 
+            const res = process(input)
+            expect(res).toEqual(3);
         });
     })
 
     describe('edge cases', () => {
-        test('description', () => {
+        test('no function is given as parameter', () => {
+            const input = 1;
+            const process = pipe([]);
+            const res = process(input);
+            expect(res).toEqual(1);
         });
     })
 })
