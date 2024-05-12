8f9a04402a048642c3d30a557385448145bd9659
Taewan Kim <taekim.dev@gmail.com>
2024-05-13 07:50:48 +0800

    add test cases for json Stringify

--- questions/javascript/jsonMethods/jsonStringify/jsonStringify.test.js
+++ questions/javascript/jsonMethods/jsonStringify/jsonStringify.test.js
@@ -2,13 +2,46 @@ const jsonStringify = require('./jsonStringify.js')
 
 describe('Json Stringify Test', () => {
     describe('standard behavior', () => {
-        test('description', () => {
-            expect().toEqual();
+        test('handles simple object', () => {
+            const input = {
+                name: "Kim"
+            };
+            const res = jsonStringify(input);
+            const expectedOutput = '{"name":"Kim"}'
+            expect(res).toEqual(expectedOutput);
+        });
+
+        test('handles a complex object', () => {
+            const input = {
+                name: "Kim",
+                address: "Korea",
+                favorite: {
+                    morning: "park",
+                    night: {
+                        everyday: "coding",
+                        holiday: "movie"
+                    }
+                }
+            };
+            const res = jsonStringify(input);
+            const expectedOutput = '{"name":"Kim","address":"Korea","favorite":{"morning":"park","night":{"everyday":"coding","holiday":"movie"}}}';
+            expect(res).toEqual(expectedOutput);
+        });
+
+        test('handles a simple array', () => {
+            const input = [Number(1), new String("2"), new Boolean(true)]
+            const res = jsonStringify(input);
+            const expectedOutput = '[1,"2",true]'; 
+            expect(res).toEqual(expectedOutput);
         });
     })
 
     describe('edge cases', () => {
-        test('description', () => {
+        test('handles empty object', () => {
+            const input = {};
+            const res = jsonStringify(input);
+            const expectedOutput = '{}'
+            expect(res).toEqual(expectedOutput);
         });
     })
 })
