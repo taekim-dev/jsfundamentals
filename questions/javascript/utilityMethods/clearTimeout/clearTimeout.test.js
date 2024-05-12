2aad2013d5179842df84e493906c8e519eaa6fe6
Taewan Kim <taekim.dev@gmail.com>
2024-05-12 09:02:18 +0800

    add test cases for clearTimeout

--- questions/javascript/utilityMethods/clearTimeout/clearTimeout.test.js
+++ questions/javascript/utilityMethods/clearTimeout/clearTimeout.test.js
@@ -2,8 +2,30 @@ const clearTimeout = require('./clearTimeout.js')
 
 describe('clearTimeout Test', () => {
     describe('standard behavior', () => {
-        test('description', () => {
-            expect().toEqual();
+        test('clear a single setTimeout', () => {
+            const func = () => {console.log('hello')};
+            setTimeout(func, 1000)
+            clearTimeout();
+
+            expect(func).toHaveBeenCalledTimes(0);
+        });
+
+        test('clear multiple setTimeouts', () => {
+            const func1 = () => {console.log('hello1')};
+            const func2 = () => {console.log('hello2')};
+            const func3 = () => {console.log('hello3')};
+
+
+            setTimeout(func1, 1000)
+            setTimeout(func2, 2000)
+            setTimeout(func3, 3000)
+
+            clearTimeout();
+
+            expect(func1).toHaveBeenCalledTimes(0);
+            expect(func1).toHaveBeenCalledTimes(0);
+            expect(func1).toHaveBeenCalledTimes(0);
+
         });
     })
 
