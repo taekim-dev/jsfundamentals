bbaea011e316d7235f3bb1313342003d0de7b86b
Taewan Kim <taekim.dev@gmail.com>
2024-05-12 18:59:23 +0800

    add event emitter implementation

--- questions/NodeJs/EventEmitter.js
+++ questions/NodeJs/EventEmitter.js
@@ -1,10 +1,27 @@
 class EventEmitter {
+
+    constructor() {
+        this.events = new Map();
+    }
+ 
     subscribe(eventName, callback) {
-        
+        if (!this.events.get(eventName)) {
+            this.events.set(eventName, []);
+        }
+        this.events.get(eventName).push(callback);
     }
     
     emit(eventName, ...args) {
-        
+        const callbacks = this.events.get(eventName);
+        if (callbacks) {
+            callbacks.forEach(func => {
+                func(args);
+            })
+        }
+    }
+
+    release(eventName) {
+        this.events.delete(eventName);
     }
 }
 
