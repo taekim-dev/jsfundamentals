0f5f1fd9dad6b9f5e91f675869f99ee5cb314c9b
Taewan Kim <taekim.dev@gmail.com>
2024-05-13 21:00:43 +0800

    json parse partial implemenation

--- questions/javascript/jsonMethods/jsonParse/jsonParse.js
+++ questions/javascript/jsonMethods/jsonParse/jsonParse.js
@@ -1,6 +1,35 @@
-function jsonParse() {
-
+function jsonParse(jsonString) {
+    jsonString = jsonString.trim();
     
+    if (jsonString === 'null') return null;
+    if (jsonString === 'true') return true;
+    if (jsonString === 'false') return false;
+
+    if (jsonString.startsWith('"') && jsonString.endsWith('"')) {
+        return jsonString.slice(1, -1);
+    }
+
+    if (!isNaN(jsonString)) {
+        return Number(jsonString);
+    }
+
+    if (jsonString.startsWith('[') && jsonString.endsWith(']')) {
+        const arrayString = jsonString.slice(1, -1);
+        const elements = arrayString.split(',').map(jsonParse);
+        return elements;
+    }
+
+    if (jsonString.startsWith('{') && jsonString.endsWith('}')) {
+        const objectString = jsonString.slice(1, -1);
+        const keyValuePairs = objectString.split(',').map(pair => pair.split(':'));
+        const obj = {};
+        keyValuePairs.forEach(([key, value]) => {
+            obj[jsonParse(key)] = jsonParse(value);
+        })
+        return obj;
+    }
+
+    throw new Error('Invalid JSON');
 }
 
 module.exports = jsonParse;
