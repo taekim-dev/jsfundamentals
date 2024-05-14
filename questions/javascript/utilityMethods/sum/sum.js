4e9b07b8e1160f956291fae84c4663c8c42a59c7
Taewan Kim <taekim.dev@gmail.com>
2024-05-14 08:01:29 +0800

    add skeleton for Sum

---
+++ questions/javascript/utilityMethods/sum/sum.js
@@ -0,0 +1,6 @@
+function sum() {
+
+}
+
+module.exports = sum;
+
---
+++ questions/javascript/utilityMethods/sum/sum.test.js
@@ -0,0 +1,14 @@
+const sum = require('./sum.js')
+
+describe('Function Name', () => {
+    describe('standard behavior', () => {
+        test('description', () => {
+            expect().toEqual();
+        });
+    })
+
+    describe('edge cases', () => {
+        test('description', () => {
+        });
+    })
+})
