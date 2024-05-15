8565c88aefddd046cfc59f04839fac5727b61f39
Taewan Kim <taekim.dev@gmail.com>
2024-05-15 08:20:47 +0800

    add sum test

--- questions/javascript/randomPractices/spreadOperator.test.js
+++ questions/javascript/randomPractices/spreadOperator.test.js
@@ -7,9 +7,22 @@ const collectAndSpread = (fn, ...args) => {
 }
 
 
-describe('Function Name', () => {
-    describe('standard behavior', () => {
+describe('Spread Operator with Function Calls', () => {
+    describe('Sum with rest parameter', () => {
+        test('handles ', () => {
+            const sum = (...args) => args.reduce((acc, curr) => acc + curr, 0);
+            expect(sum(1, 2)).toBe(3);
+            expect(sum(1, 2, 3, 4, 5)).toBe(15);
+            expect(sum()).toBe(0);
+        });
+
         test('description', () => {
+            
+            expect().toEqual();
+        });
+
+        test('description', () => {
+            
             expect().toEqual();
         });
     })
