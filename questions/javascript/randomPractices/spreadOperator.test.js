9c17c005ea842dbf05bdf451dd74612e140c2d8f
Taewan Kim <taekim.dev@gmail.com>
2024-05-15 08:39:18 +0800

    add more test cases

--- questions/javascript/randomPractices/spreadOperator.test.js
+++ questions/javascript/randomPractices/spreadOperator.test.js
@@ -1,10 +1,3 @@
-
-const collectAndSpread = (fn, ...args) => {
-    const collectedArgs = args;
-    return fn(...collectedArgs)
-}
-
-
 describe('Spread Operator with Function Calls', () => {
     describe('Sum with rest parameter', () => {
         test('handles ', () => {
@@ -29,6 +22,52 @@ describe('Spread Operator with Function Calls', () => {
             
             expect(res).toEqual(6);
         });
+
+        test('collecting and spreading arguments into another function', () => {
+            const sum = (a, b, c) => a + b + c;
+            const collectAndSpread = (fn, ...args) => {
+                const collectedArgs = args; // collectedArgs is Array
+                return fn(...collectedArgs);
+            }
+            const args = [1, 2, 3];
+        
+            expect(collectAndSpread(sum, ...args)).toEqual(6);
+        });
+
+        test('collecting arguments and using them directly', () => {
+            const multiply = (...args) => args.reduce((acc, curr) => acc * curr, 1);
+
+            expect(multiply(1, 2, 3)).toBe(6);
+            expect(multiply(4, 5)).toBe(20);
+        });
+
+        test('spreading arguments from array into a function call', () => {
+            const max = Math.max;
+            const numbers = [10, 20, 30, 41, 15];
+            
+            expect(max(...numbers)).toBe(41);
+        });
+
+        test('combine arrays', () => {
+            const array1 = [1, 2, 3];
+            const array2 = [3, 4, 5];
+            const combined = (args1, args2) => [...args1, ...args2];
+            
+            expect(combined(array1, array2)).toEqual([1, 2, 3, 3, 4, 5]);
+        });
+
+        test('spread strings', () => {
+            const toArray = (args) => [...args];
+            const stringInput = "hello"
+            
+            expect(toArray(stringInput)).toEqual(['h','e','l','l','o']);
+        });
+
+        test('add to array', () => {
+            const addToArray = (arr, ...args) => [...arr, ...args];
+            expect(addToArray([1, 2], 3, 4, 5)).toEqual([1, 2, 3, 4 ,5]);
+        });
+
     })
 
     describe('edge cases', () => {
