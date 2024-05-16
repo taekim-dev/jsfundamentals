53a3dbcf4be9fcd387e0bbfa82d4451ccbcdf88c
Taewan Kim <taekim.dev@gmail.com>
2024-05-17 07:45:48 +0800

    add a simple tree test case

--- questions/javascript/randomPractices/serializeTree/serializeTree.test.js
+++ questions/javascript/randomPractices/serializeTree/serializeTree.test.js
@@ -1,14 +1,44 @@
 const { serialize, deserialize } = require('./serializeTree.js')
 
-describe('Function Name', () => {
+class Node {
+  constructor(val) {
+    this.value = val
+    this.left = null
+    this.right = null
+  }
+}
+
+function createBinaryTree (array) {
+
+}
+
+describe('Serialize and Deserialize Binrary Tree', () => {
     describe('standard behavior', () => {
+        test('serialize a simple tree', () => {
+            const root = new Node(1);
+            const left = new Node(2);
+            const right = new Node(3);
+            root.left = left;
+            root.right = right;
+
+            const serizliedTree = serialize(root);
+            expect(serizliedTree).toEqual([1, 2, 3]);
+
+            const deserializedTree = deserialize(serizliedTree);
+            expect(deserializedTree).toEqual(root);
+        });
+
+        test('serialize a complex tree', () => {
+            expect().toEqual();
+        });
+
         test('description', () => {
             expect().toEqual();
         });
     })
 
     describe('edge cases', () => {
-        test('description', () => {
+        test('serialize an empty tree', () => {
         });
     })
 })
