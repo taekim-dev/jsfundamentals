abd072e873713fcdc4a94b0dbba2060ea1d0b5cf
Taewan Kim <taekim.dev@gmail.com>
2024-05-17 07:33:34 +0800

    add skeleton for serializeTre

---
+++ questions/javascript/randomPractices/serializeTree/serializeTree.js
@@ -0,0 +1,33 @@
+// class Node {
+//   value: number
+//   left: null | Node
+//   right: null | Node
+//   constructor(val) {
+//     this.value = val
+//     this.left = null
+//     this.right = null
+//   }
+// }
+
+/**
+ * @param {Node} root
+ * @return {string}
+ */
+function serialize(root) {
+
+}
+  
+/**
+ * @param {string} str
+ * @return {Node}
+ */
+function deserialize(str) {
+
+}
+  
+  
+module.exports = {
+    serialize,
+    deserialize
+};
+  
---
+++ questions/javascript/randomPractices/serializeTree/serializeTree.test.js
@@ -0,0 +1,14 @@
+const { serialize, deserialize } = require('./serializeTree.js')
+
+describe('Function Name', () => {
+    describe('standard behavior', () => {
+        test('description', () => {
+            expect().toEqual();
+        });
+    })
+
+    describe('edge cases', () => {
+        test('description', () => {
+        });
+    })
+})
