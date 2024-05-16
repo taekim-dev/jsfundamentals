b4d870ed4a3bda2ec446dd76647bbe6d44e977ff
Taewan Kim <taekim.dev@gmail.com>
2024-05-16 08:50:44 +0800

    implement primise any

--- questions/javascript/promiseMethod/any/any.js
+++ questions/javascript/promiseMethod/any/any.js
@@ -1,7 +1,31 @@
+
 function any(promises) {
+    // empty promise => reject
+    if (promises.length === 0) {
+        return Promise.reject(new AggregateError([], "no promise given"))
+    }
+
+    return new Promise((resolve, reject) => {
+        const errors = [];
+        let rejectCount = 0;
+        
+        promises.forEach(promise => {
+            Promise.resolve(promise)
+                .then(value => {
+                    resolve(value);
+                }).catch(error => {
+                    rejectCount++;
+                    errors.push(error);
+
+                    if(promises.length === rejectCount) {
+                        reject(new AggregateError(errors, "all promises are rejected"));
+                    }
+                })
+        })
+
+    })
 
 }
 
-
-module.exports = methodName;
+module.exports = any;
 
