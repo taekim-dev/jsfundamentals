99f0be94ae73a067324b304b35e4e065e9d79d11
Taewan Kim <taekim.dev@gmail.com>
2024-05-16 08:28:55 +0800

    add multiple promises test cases

--- questions/javascript/promiseMethod/any/any.test.js
+++ questions/javascript/promiseMethod/any/any.test.js
@@ -22,16 +22,41 @@ describe('Promise any Test', () => {
             });
         });
 
-        test('handles mutilple promises resolved', () => {
-            expect().toEqual();
+        test('handles mutilple promises resolved synchrounously', () => {
+            const promise1 = Promise.resolve(1);
+            const promise2 = Promise.resolve(2);
+            const promise3 = Promise.resolve(3);
+
+            const res = any([promise1, promise2, promise3]);
+            return res.then(value => {
+                expect(value).toEqual(1);
+            })
         });
 
         test('handles mutilple promises resolved asynchronously', () => {
-            expect().toEqual();
+            const promise1 = new Promise((resolve) => {
+                setTimeout(resolve, 200, "first")
+            })
+            const promise2 = new Promise((resolve) => setTimeout(resolve, 100, "second"));
+            const promise3 = new Promise((resolve) => setTimeout(resolve, 300, "third"));
+
+            const res = any([promise1, promise2, promise3]);
+            return res.then(value => {
+                expect(value).toEqual("second");
+            })
         });
 
         test('handles all promises failed', () => {
-            expect().toEqual();
+            const promise1 = Promise.reject(1);
+            const promise2 = Promise.reject(2);
+            const promise3 = Promise.reject(3);
+
+            const res = any([promise1, promise2, promise3]);
+
+            return res.catch(error => {
+                expect(error).toBeInstanceOf(AggregateError);
+                expect(error.errors).toEqual([1, 2, 3]);
+            });
         });
     })
 
