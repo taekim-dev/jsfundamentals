773d7946ebe4e488e09ae4fd43cf0dbbbeb5b188
Taewan Kim <taekim.dev@gmail.com>
2024-05-17 07:51:37 +0800

    add complex test case

--- questions/javascript/randomPractices/serializeTree/serializeTree.js
+++ questions/javascript/randomPractices/serializeTree/serializeTree.js
@@ -1,14 +1,3 @@
-// class Node {
-//   value: number
-//   left: null | Node
-//   right: null | Node
-//   constructor(val) {
-//     this.value = val
-//     this.left = null
-//     this.right = null
-//   }
-// }
-
 /**
  * @param {Node} root
  * @return {string}
--- questions/javascript/randomPractices/serializeTree/serializeTree.test.js
+++ questions/javascript/randomPractices/serializeTree/serializeTree.test.js
@@ -28,17 +28,52 @@ describe('Serialize and Deserialize Binrary Tree', () => {
             expect(deserializedTree).toEqual(root);
         });
 
-        test('serialize a complex tree', () => {
-            expect().toEqual();
+        test('serialize a simple tree with null values', () => {
+            const root = new Node(1);
+            const left = new Node(2);
+            const right = new Node(3);
+            root.left = left;
+            left.right = right;
+
+            const serizliedTree = serialize(root);
+            expect(serizliedTree).toEqual([1, 2, null, null, 3]);
+
+            const deserializedTree = deserialize(serizliedTree);
+            expect(deserializedTree).toEqual(root);
         });
 
-        test('description', () => {
-            expect().toEqual();
+        test('serialize a complex tree', () => {
+            const root = new Node(1);
+
+            root.left = new Node(2);
+            root.right = new Node(3);
+
+            root.left.left = new Node(4);
+
+            root.left.left.left = new Node(6);
+            root.left.left.right = new Node(7);
+
+            root.right.right = new Node(5);
+
+            root.right.right.left = new Node(8);
+            root.right.right.left.right = new Node(9);
+
+            const serizliedTree = serialize(root);
+            expect(serizliedTree).toEqual([1,2,3,4,null,null,5,6,7,8,null,null,null,null,9]);
+
+            const deserializedTree = deserialize(serizliedTree);
+            expect(deserializedTree).toEqual(root);
         });
     })
 
     describe('edge cases', () => {
         test('serialize an empty tree', () => {
+            const root = null;
+            const serizliedTree = serialize(root);
+            expect(serizliedTree).toEqual(null);
+
+            const deserializedTree = deserialize(serizliedTree);
+            expect(deserializedTree).toEqual(root);
         });
     })
 })
