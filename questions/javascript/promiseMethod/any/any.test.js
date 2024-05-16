460e1187ff10ccd3897535d10ecd1e59aa16e82a
Taewan Kim <taekim.dev@gmail.com>
2024-05-16 08:12:11 +0800

    add skeleton for test cases

--- questions/javascript/promiseMethod/any/any.test.js
+++ questions/javascript/promiseMethod/any/any.test.js
@@ -2,7 +2,23 @@ const any = require('./any.js')
 
 describe('Promise any Test', () => {
     describe('standard behavior', () => {
-        test('description', () => {
+        test('handles a single promise resolved', () => {
+            expect().toEqual();
+        });
+
+        test('handles a single promise rejected', () => {
+            expect().toEqual();
+        });
+
+        test('handles mutilple promises resolved', () => {
+            expect().toEqual();
+        });
+
+        test('handles mutilple promises resolved asynchronously', () => {
+            expect().toEqual();
+        });
+
+        test('handles all promises failed', () => {
             expect().toEqual();
         });
     })
