b6281a0444017dbdf4ce3b60df1ba2be9db46d83
Tae Kim <taekim.dev@gmail.com>
2024-05-18 16:35:14 +0800

    implement object assign

--- questions/javascript/utilityMethods/objectAssign/objectAssign.js
+++ questions/javascript/utilityMethods/objectAssign/objectAssign.js
@@ -1,6 +1,18 @@
-function objectAssign() {
+function objectAssign(target, source) {
+    // modify the original target, by adding source 
+    // if duplicate key found, replace with the value of source
 
-    
+    const sourceKeys = Object.keys(source);
+    sourceKeys.forEach(key => {
+        if (key in target) {
+            if (typeof target[key] === 'object') {
+                target[key] = objectAssign(target[key], source[key]);
+            } else {
+                target[key] = source[key];
+            }
+        }
+    })
+    return target;
 }
 
 module.exports = objectAssign;
