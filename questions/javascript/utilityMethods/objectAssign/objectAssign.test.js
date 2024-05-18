2a2deea1cd6f8dbfdd34f0ad17263b7c1b2df20c
Tae Kim <taekim.dev@gmail.com>
2024-05-18 16:30:26 +0800

    add test cases for object assign

--- questions/javascript/utilityMethods/objectAssign/objectAssign.test.js
+++ questions/javascript/utilityMethods/objectAssign/objectAssign.test.js
@@ -1,14 +1,75 @@
 const objectAssign = require('./objectAssign.js')
 
-describe('Function Name', () => {
+describe('Object Assign Test', () => {
     describe('standard behavior', () => {
-        test('description', () => {
-            expect().toEqual();
+        test('handles a simple targe and source', () => {
+            const target = { name: "Kim", age: "12"};
+            const source = { age: 30, country: "US"};
+
+            const assignedTarget = objectAssign(target, source);
+            expect(assignedTarget).toEqual(target);
+            expect(assignedTarget.name).toEqual("Kim");
+            expect(assignedTarget.age).toEqual("30");
+            expect(assignedTarget.country).toEqual("US");
+
         });
+
+        test('handles a complex targe and source', () => {
+            const target = { 
+                name: "Kim", 
+                age: "12",
+                address : {
+                    country : "US",
+                    county: "Suffolk",
+                    zipcode: "11790",
+                    status : {
+                        visa: "F-1"
+                    }
+                }
+            };
+            const source = { 
+                name: "Kim", 
+                age: 30,
+                address : {
+                    zipcode: "10001",
+                    status : {
+                        visa: "O-1"
+                    }
+                }
+            };
+
+            const assignedTarget = objectAssign(target, source);
+            expect(assignedTarget).toEqual(target);
+            expect(assignedTarget.name).toEqual("Kim");
+            expect(assignedTarget.age).toEqual(30);
+            expect(assignedTarget.address.country).toEqual("US");
+            expect(target.address.zipcode).toEqual("10001");
+            expect(target.address.status.visa).toEqual("O-1");
+
+        });
+
     })
 
     describe('edge cases', () => {
-        test('description', () => {
+        test('handles an empty target', () => {
+            const target = {};
+            const source = { a : 1, b : 2};
+            const assignedTarget = objectAssign(target, source);
+
+            expect(target).toEqual(assignedTarget);
+            expect(target.a).toEqual(1);
+            expect(assignedTarget.b).toEqual(2);
+
+        });
+
+        test('handles an empty source', () => {
+            const target = {a : 1, b : 2};
+            const source = {};
+            const assignedTarget = objectAssign(target, source);
+
+            expect(target).toEqual(assignedTarget);
+            expect(target.a).toEqual(1);
+            expect(assignedTarget.b).toEqual(2);
         });
     })
 })
