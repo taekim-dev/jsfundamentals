9f1e47da4a28db995d3515b550e606cb4f79d36d
Tae Kim <taekim.dev@gmail.com>
2024-05-18 09:27:05 +0800

    modify the test cases

--- questions/javascript/randomPractices/firstBadVersion/firstBadVersion.js
+++ questions/javascript/randomPractices/firstBadVersion/firstBadVersion.js
@@ -7,6 +7,8 @@
  * @return {(v: number) => number}
  */
 function firstBadVersion(isBad) {
+
+    
 	// firstBadVersion receive a check function isBad
   // and should return a closure which accepts a version number(integer)
   return (version) => {
--- questions/javascript/randomPractices/firstBadVersion/firstBadVersion.test.js
+++ questions/javascript/randomPractices/firstBadVersion/firstBadVersion.test.js
@@ -3,26 +3,18 @@ const firstBadVersion = require('./firstBadVersion.js')
 describe('First Bad Version Test', () => {
     describe('standard behavior', () => {
         test('handles when no bad version with a simple input', () => {
-            const isBad = (x) => x / 2 === 0;
-            const findOddNumbers = firstBadVersion(isBad)
-            const input = 3;
-            const res = findOddNumbers(input);
+            const isBad = (x) => x > 4;
+            const badVersion = firstBadVersion(isBad);
+            const res = badVersion(3);
 
             expect(res).toEqual(-1);
         });
 
-        test('handles when no bad with a complex input', () => {
-            const isBad = (x) => x / 2 === 0;
-            const findOddNumbers = firstBadVersion(isBad)
-            const input = [1, 3, 5, 7];
-            input.forEach(num => {
-                const res = findOddNumbers(num)
-                expect(res).toEqual(-1);
-            })
-        });
-
         test('handles when bad version with a simple input', () => {
-            expect().toEqual();
+            const isBad = (x) => x % 2 === 0;
+            const badVersion = firstBadVersion(isBad);
+            const res = badVersion(3);
+            expect(res).toEqual(1);
         });
 
         test('handles when bad version with a complex input', () => {
