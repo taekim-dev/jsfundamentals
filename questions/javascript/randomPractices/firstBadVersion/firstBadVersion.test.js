69f0b2ea882caacd3c8be1f830455355615cffee
Tae Kim <taekim.dev@gmail.com>
2024-05-18 09:22:29 +0800

    add no bad version test cases

--- questions/javascript/randomPractices/firstBadVersion/firstBadVersion.test.js
+++ questions/javascript/randomPractices/firstBadVersion/firstBadVersion.test.js
@@ -2,7 +2,30 @@ const firstBadVersion = require('./firstBadVersion.js')
 
 describe('First Bad Version Test', () => {
     describe('standard behavior', () => {
-        test('description', () => {
+        test('handles when no bad version with a simple input', () => {
+            const isBad = (x) => x / 2 === 0;
+            const findOddNumbers = firstBadVersion(isBad)
+            const input = 3;
+            const res = findOddNumbers(input);
+
+            expect(res).toEqual(-1);
+        });
+
+        test('handles when no bad with a complex input', () => {
+            const isBad = (x) => x / 2 === 0;
+            const findOddNumbers = firstBadVersion(isBad)
+            const input = [1, 3, 5, 7];
+            input.forEach(num => {
+                const res = findOddNumbers(num)
+                expect(res).toEqual(-1);
+            })
+        });
+
+        test('handles when bad version with a simple input', () => {
+            expect().toEqual();
+        });
+
+        test('handles when bad version with a complex input', () => {
             expect().toEqual();
         });
     })
