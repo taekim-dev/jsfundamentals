d1331225a510b2b6979833e4af05524e425fbb41
Tae Kim <taekim.dev@gmail.com>
2024-05-20 07:54:10 +0800

    add skeleton for `sequence`

---
+++ questions/javascript/utilityMethods/sequence/sequence.js
@@ -0,0 +1,20 @@
+/*
+type Callback = (error: Error, data: any) => void
+
+type AsyncFunc = (
+   callback: Callback,
+   data: any
+) => void
+
+*/
+
+/**
+ * @param {AsyncFunc[]} funcs
+ * @return {(callback: Callback) => void}
+ */
+function sequence(funcs){
+    // your code here
+  }
+
+module.exports = sequence;
+
---
+++ questions/javascript/utilityMethods/sequence/sequence.test.js
@@ -0,0 +1,14 @@
+const sequence = require('./sequence.js')
+
+describe('Function Name', () => {
+    describe('standard behavior', () => {
+        test('description', () => {
+            expect().toEqual();
+        });
+    })
+
+    describe('edge cases', () => {
+        test('description', () => {
+        });
+    })
+})
