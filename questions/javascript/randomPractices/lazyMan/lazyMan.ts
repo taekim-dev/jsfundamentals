59aa21d597d2f21987c8f3cf4c4f257605c52d5b
Tae Kim <taekim.dev@gmail.com>
2024-05-19 15:29:39 +0800

    add a typescript version lazyman

--- questions/javascript/randomPractices/lazyMan/lazyMan.ts
+++ questions/javascript/randomPractices/lazyMan/lazyMan.ts
@@ -5,7 +5,52 @@ interface Laziness {
 }
 
 function lazyMan(name: string, logFn: (log: string) => void) : Laziness{
-  return this as Laziness;
+  
+  const tasks : Function[] = [];
+  
+  function next() : void {
+    const task = tasks.shift();
+    if (task) task();
+  }
+
+  tasks.push(() => {
+    logFn(`Hi, I'm ${name}`);
+    next();
+  })
+
+  const lazyiness : Laziness = {
+    sleep: (time: number) => {
+      tasks.push(() => {
+        setTimeout(() => {
+          logFn(`Slept for ${time} seconds`);
+          next();
+        }, time * 1000);
+      })
+      return lazyiness;
+    },
+
+    sleepFirst: (time: number) => {
+      tasks.unshift(() => {
+        setTimeout(() => {
+          logFn(`Slept for ${time} seconds`);
+          next();
+        }, time * 1000);
+      })
+      return lazyiness;
+    },
+
+    eat: (food: String) => {
+      tasks.push(() => {
+        logFn(`Eating ${food}`);
+      })
+      return lazyiness;
+    }
+  }
+
+  setTimeout(next, 0);
+
+  return lazyiness;
+
 }
 
 module.exports = lazyMan;
