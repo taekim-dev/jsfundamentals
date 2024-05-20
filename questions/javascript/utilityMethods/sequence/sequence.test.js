a4429ab752bd13d700d2aec9d933c6ebe7ae0095
Tae Kim <taekim.dev@gmail.com>
2024-05-20 08:01:27 +0800

    add simple test cases

--- questions/javascript/utilityMethods/sequence/sequence.test.js
+++ questions/javascript/utilityMethods/sequence/sequence.test.js
@@ -1,9 +1,39 @@
 const sequence = require('./sequence.js')
 
-describe('Function Name', () => {
+describe('Sequence Test', () => {
     describe('standard behavior', () => {
-        test('description', () => {
-            expect().toEqual();
+        test('handles a single async function', () => {
+            const asyncTimes2 = (callback, num) => {
+                setTimeout(() => callback(null, num * 2), 100);
+            }
+            const times2 = sequence([asyncTimes2])
+            let res;
+            times2((error, data) => {
+                res = data;
+            }, 1)
+
+            expect(res).toEqual(2);
+        });
+
+        test('handles the same async functions called multiple times', () => {
+            function asyncTimes2(callback, num) {
+                setTimeout(() => {
+                    callback(null, num * 2);
+                }, 100)
+            }
+
+            const times4 = sequence([
+                asyncTimes2,
+                asyncTimes2,
+                asyncTimes2,
+            ])
+
+            let res;
+            times4((error, data) => {
+                res = data;
+            }, 1)
+
+            expect(res).toEqual(8);
         });
     })
 
