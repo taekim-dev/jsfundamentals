f9eec4d82c1c0f7e1e76b9602aa49e01a41415f7
Tae Kim <taekim.dev@gmail.com>
2024-05-22 08:45:10 +0800

    add update: $push

--- questions/javascript/utilityMethods/update/update.js
+++ questions/javascript/utilityMethods/update/update.js
@@ -1,9 +1,25 @@
-function methodName() {
+function update(data, command) {
+    if (typeof command !== 'object' || command === null) {
+        throw new Error('command must be object')
+    }
 
+    const result = Array.isArray(data) ? [...data] : {...data};
+
+    for (const key in command) {
+        if (command.hasOwnProperty(key)) {
+            if (key === '$push') {
+                if (!Array.isArray(result)) {
+                    throw new Error('$push only applies to arrays')
+                } else {
+                    result.push(...command[key]);
+                }
+            }
+        }
+    }
+
+    return result;
 }
 
-// Function(Array).prototype.methodName = function() {
-// }
 
-module.exports = methodName;
+module.exports = update;
 
