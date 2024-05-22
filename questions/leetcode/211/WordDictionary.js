ad443e0109c93bddbefaf73808b2b4857625a05f
Tae Kim <taekim.dev@gmail.com>
2024-05-22 22:07:12 +0800

    add test cases

--- questions/leetcode/211/WordDictionary.js
+++ questions/leetcode/211/WordDictionary.js
@@ -25,3 +25,22 @@ WordDictionary.prototype.search = function(word) {
  * obj.addWord(word)
  * var param_2 = obj.search(word)
  */
+
+const wordDictionary = new WordDictionary();
+wordDictionary.addWord("bad");
+wordDictionary.addWord("dad");
+wordDictionary.addWord("mad");
+console.log (wordDictionary.search("pad") === false)
+console.log (wordDictionary.search("bad") === true)
+console.log (wordDictionary.search(".ad") === true)
+console.log (wordDictionary.search("b..") === true)
+
+const wordDictionary2 = new WordDictionary();
+wordDictionary2.addWord("cat");
+wordDictionary2.addWord("push");
+wordDictionary2.addWord("dog");
+console.log (wordDictionary2.search("g") === true)
+console.log (wordDictionary2.search("put") === false)
+console.log (wordDictionary2.search("p..h") === true)
+console.log (wordDictionary2.search("..t") === true)
+console.log (wordDictionary2.search("b..") === false)
