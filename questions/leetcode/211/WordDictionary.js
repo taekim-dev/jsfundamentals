660bb69cd6ffa4bcd2fe610e3b57893fe5bdcdfb
Tae Kim <taekim.dev@gmail.com>
2024-05-22 23:31:09 +0800

    add implemenation

--- questions/leetcode/211/WordDictionary.js
+++ questions/leetcode/211/WordDictionary.js
@@ -1,6 +1,7 @@
 
 var WordDictionary = function() {
-    
+    this.dict = {};
+    // [a] : {[b] : { [c, d]}...}
 };
 
 /** 
@@ -8,7 +9,15 @@ var WordDictionary = function() {
  * @return {void}
  */
 WordDictionary.prototype.addWord = function(word) {
-    
+    let curr = this.dict;
+    for (let i = 0; i < word.length; i++) {
+        const char = word[i];
+        if (!curr[char]) {
+            curr[char] = {};
+        }
+        curr = curr[char];
+    }
+    curr.isEnd = true;
 };
 
 /** 
@@ -16,7 +25,29 @@ WordDictionary.prototype.addWord = function(word) {
  * @return {boolean}
  */
 WordDictionary.prototype.search = function(word) {
-    
+    const searchInNode = (node, word, index) => {
+        if (index === word.length) {
+            return node.isEnd === true;
+        }
+
+        const char = word[index];
+        
+        if (char === '.') {
+            for(let key in node) {
+                if (key !== 'isEnd' && searchInNode(node[key], word, index + 1)){
+                    return true;
+                }
+            }
+        } else {
+            if (node[char] && searchInNode(node[char], word, index + 1)) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
+    return searchInNode(this.dict, word, 0);
 };
 
 /** 
@@ -39,8 +70,17 @@ const wordDictionary2 = new WordDictionary();
 wordDictionary2.addWord("cat");
 wordDictionary2.addWord("push");
 wordDictionary2.addWord("dog");
-console.log (wordDictionary2.search("g") === true)
+console.log (wordDictionary2.search("g") === false)
 console.log (wordDictionary2.search("put") === false)
 console.log (wordDictionary2.search("p..h") === true)
 console.log (wordDictionary2.search("..t") === true)
 console.log (wordDictionary2.search("b..") === false)
+
+const worldDictionary3 = new WordDictionary();
+worldDictionary3.addWord("abcdefghijklmnop");
+worldDictionary3.addWord("qrstuvwxyz");
+console.log (worldDictionary3.search("defghijkl") === false)
+console.log (worldDictionary3.search("vwxy") === false)
+console.log (worldDictionary3.search("pqrstuv") === false)
+console.log (worldDictionary3.search("...t") === false)
+console.log (worldDictionary3.search("....t") === false)
