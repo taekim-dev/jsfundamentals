9634b9e357bd89b2442642b898631741a3da6568
Tae Kim <taekim.dev@gmail.com>
2024-05-23 08:39:47 +0800

    add test cases for the virtual DOM

--- questions/javascript/randomPractices/VirtualDom/virtualDom.test.js
+++ questions/javascript/randomPractices/VirtualDom/virtualDom.test.js
@@ -1,15 +1,106 @@
 const { virtualize, render } = require('./virtualDom.js')
-// require('./methodName.js')
 
-describe('Function Name', () => {
-    describe('standard behavior', () => {
-        test('description', () => {
-            expect().toEqual();
-        });
-    })
+test('virtualize: converts a simple DOM element to an object', () => {
+  document.body.innerHTML = '<div><h1> this is </h1><p class="paragraph"> a <button> button </button> from <a href="https://bfe.dev"><b>BFE</b>.dev</a></p></div>';
+  const element = document.querySelector('div');
 
-    describe('edge cases', () => {
-        test('description', () => {
-        });
-    })
-})
+  const expected = {
+    type: 'div',
+    props: {
+      children: [
+        {
+          type: 'h1',
+          props: {
+            children: ' this is '
+          }
+        },
+        {
+          type: 'p',
+          props: {
+            className: 'paragraph',
+            children: [
+              ' a ',
+              {
+                type: 'button',
+                props: {
+                  children: ' button '
+                }
+              },
+              ' from',
+              {
+                type: 'a',
+                props: {
+                  href: 'https://bfe.dev',
+                  children: [
+                    {
+                      type: 'b',
+                      props: {
+                        children: 'BFE'
+                      }
+                    },
+                    '.dev'
+                  ]
+                }
+              }
+            ]
+          }
+        }
+      ]
+    }
+  };
+
+  const result = virtualize(element);
+  expect(result).toEqual(expected);
+});
+
+test('render: converts an object literal to a DOM element', () => {
+  const obj = {
+    type: 'div',
+    props: {
+      children: [
+        {
+          type: 'h1',
+          props: {
+            children: ' this is '
+          }
+        },
+        {
+          type: 'p',
+          props: {
+            className: 'paragraph',
+            children: [
+              ' a ',
+              {
+                type: 'button',
+                props: {
+                  children: ' button '
+                }
+              },
+              ' from',
+              {
+                type: 'a',
+                props: {
+                  href: 'https://bfe.dev',
+                  children: [
+                    {
+                      type: 'b',
+                      props: {
+                        children: 'BFE'
+                      }
+                    },
+                    '.dev'
+                  ]
+                }
+              }
+            ]
+          }
+        }
+      ]
+    }
+  };
+
+  const element = render(obj);
+  const expectedHTML = '<div><h1> this is </h1><p class="paragraph"> a <button> button </button> from <a href="https://bfe.dev"><b>BFE</b>.dev</a></p></div>';
+
+  expect(element.outerHTML).toEqual(expectedHTML);
+});
