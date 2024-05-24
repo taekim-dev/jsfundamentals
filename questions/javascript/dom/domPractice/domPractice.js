fc2b75b4b82998addbfb949f045e5fd07f280814
Tae Kim <taekim.dev@gmail.com>
2024-05-24 08:45:06 +0800

    add create and append element

---
+++ questions/javascript/dom/domPractice/domPractice.js
@@ -0,0 +1,14 @@
+/**
+ * Creates a new element, sets its text content, and appends it to the specified parent.
+ * @param {string} tagName
+ * @param {string} textContent
+ * @param {HTMLElement} parent
+ * @return {HTMLElement}
+ */
+
+function createAndAppendElement(tagName, textContent, parent) {
+    const newElement = document.createElement(tagName);
+    newElement.textContent = textContent;
+    parent.appendChild(newElement);
+    return newElement;
+}
