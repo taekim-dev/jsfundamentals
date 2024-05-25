f00a7716532d73965c6188f0f6c5751da91eaf67
Tae Kim <taekim.dev@gmail.com>
2024-05-25 09:32:18 +0800

    implement palindrome

--- questions/javascript/randomPractices/isPalindrome/isPalindrome.js
+++ questions/javascript/randomPractices/isPalindrome/isPalindrome.js
@@ -1,1 +1,6 @@
 
+export function isPalindrome(str) {
+    const cleanedString = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    const reversedString = cleanedString.split('').reverse().join('');
+    return cleanedString === reversedString;
+}
