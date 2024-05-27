8a3f63134fecbe60782bc280f7d83da264dd6e6c
Tae Kim <taekim.dev@gmail.com>
2024-05-27 08:59:41 +0800

    cleanup and fix the function

--- questions/javascript/utilityMethods/throttlePromises/throttlePromises.js
+++ questions/javascript/utilityMethods/throttlePromises/throttlePromises.js
@@ -4,44 +4,37 @@
  * @param {number} max
  * @return {Promise}
  */
-function throttlePromises(funcs, max){
+function throttlePromises(funcs, max) {
   const res = new Array(funcs.length).fill(null);
   let activeCount = 0;
   let index = 0;
 
   return new Promise((resolve, reject) => {
-    function runNext() {
-      // all tasks completed
-      if (index >= funcs.length && activeCount === 0) {
-        resolve(res);
-        return;
+      function runNext() {
+          // If all tasks are completed, resolve the promise
+          if (index >= funcs.length && activeCount === 0) {
+              resolve(res);
+              return;
+          }
+
+          // Start tasks while the number of active tasks is less than max
+          while (activeCount < max && index < funcs.length) {
+              const currentIndex = index++;
+              const promise = funcs[currentIndex]();
+
+              activeCount++;
+              promise.then(result => {
+                  res[currentIndex] = result;
+                  activeCount--;
+                  runNext();
+              }).catch(error => {
+                  reject(error);
+              });
+          }
       }
 
-      // start the task while the number of active tasks is less than max
-      while (activeCount < max && index.funcs.length) {
-        const currentIndex = index++;
-        const promise = funcs[currentIndex]();
-
-        activeCount++;
-        promise.then(result => {
-          res[currentIndex] = result;
-          activeCount--;
-          runNext();
-        }).catch(error => {
-          reject(error);
-        });
-      }
-    }
-
-    runNext();
-  })
-
+      runNext();
+  });
 }
 
 module.exports = throttlePromises;
-
-throttleAsync(callApis, 5).then((data) => {
-  // the data is the same as `Promise.all` 
-}).catch((err) => {
-  // any error occurs in the callApis would be relayed here
-})
