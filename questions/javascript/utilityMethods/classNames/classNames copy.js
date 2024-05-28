49ac04491f750e4b06c389f3ac369173593973fa
Tae Kim <taekim.dev@gmail.com>
2024-05-28 23:00:52 +0800

    add skeleton for classNames

---
+++ questions/javascript/utilityMethods/classNames/classNames copy.js
@@ -0,0 +1,7 @@
+/**
+ * @param {...(any|Object|Array<any|Object|Array>)} args
+ * @return {string}
+ */
+export default function classNames(...args) {
+    throw 'Not implemented!';
+}
---
+++ questions/javascript/utilityMethods/classNames/classNames.test.js
@@ -0,0 +1,14 @@
+const classNames = require('./classNames.js')
+
+describe('classNames Test', () => {
+    describe('standard behavior', () => {
+        test('description', () => {
+            expect().toEqual();
+        });
+    })
+
+    describe('edge cases', () => {
+        test('description', () => {
+        });
+    })
+})
