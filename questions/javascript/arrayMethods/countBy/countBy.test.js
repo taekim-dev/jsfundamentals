3fd66e85ef2834634300a87109f5284654e72d9b
Tae Kim <taekim.dev@gmail.com>
2024-05-28 08:09:36 +0800

    add more test cases

--- questions/javascript/arrayMethods/countBy/countBy.test.js
+++ questions/javascript/arrayMethods/countBy/countBy.test.js
@@ -1,41 +1,35 @@
-const countBy = require('./countBy.js')
+import countBy from "./countBy.js";
 
-describe('Function Name', () => {
+describe('countBy', () => {
     describe('standard behavior', () => {
         test('handles an array input', () => {
             const input = [6.2, 4.3, 6.9];
             const res = countBy(input, Math.floor); 
             expect(res['6']).toEqual(2);
             expect(res['4']).toEqual(1);
-            expect(res['5']).toEqual(undefined);
+            expect(res['5']).toBeUndefined();
         });
 
         test('handles an object input', () => {
-            const input = [{ n : 3, n : 5, n : 3}]
+            const input = [{ n: 3 }, { n: 5 }, { n: 3 }];
             const res = countBy(input, (o) => o.n); 
 
             expect(res['3']).toEqual(2);
             expect(res['5']).toEqual(1);
         });
-    })
+    });
 
     describe('edge cases', () => {
         test('handles an empty input', () => {
-            
+            const input = [];
+            const res = countBy(input, () => {});
+            expect(res).toEqual({});
         });
 
-        test('handles an undefined input', () => {
+        test('handles objects with undefined properties', () => {
+            const input = [{ n: 1 }, { n: 2 }];
+            const res = countBy(input, (o) => o.m);
+            expect(res['undefined']).toEqual(2);
         });
-    })
-})
-
-
-countBy([6.1, 4.2, 6.3], Math.floor);
-// => { '4': 1, '6': 2 }
-
-countBy([{ n: 3 }, { n: 5 }, { n: 3 }], (o) => o.n);
-// => { '3': 2, '5': 1 }
-
-countBy([], (o) => o); // => {}
-
-countBy([{ n: 1 }, { n: 2 }], (o) => o.m); // => { undefined: 2 }
+    });
+});
