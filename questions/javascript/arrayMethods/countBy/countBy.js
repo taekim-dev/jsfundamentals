e172332ee533c7cd9e763c3db6575268f8327a12
Tae Kim <taekim.dev@gmail.com>
2024-05-28 08:21:39 +0800

    use reduce for the solution

--- questions/javascript/arrayMethods/countBy/countBy.js
+++ questions/javascript/arrayMethods/countBy/countBy.js
@@ -3,23 +3,35 @@
  * @param {Function} iteratee The function invoked per iteration.
  * @returns {Object} Returns the composed aggregate object.
  */
+// export default function countBy(array, iteratee) {
+//     const counter = {};
+
+//     array.forEach(element => {
+//         let key = iteratee(element)
+//         if (key !== null && key !== undefined) {
+//             key = key.toString();
+//         } else {
+//             key = 'undefined';
+//         }
+
+//         if (counter.hasOwnProperty(key)) {
+//             counter[key]++;
+//         } else {
+//             counter[key] = 1;
+//         }
+//     })
+
+//     return counter;
+// }
+
 export default function countBy(array, iteratee) {
-    const counter = {};
+    return array.reduce((counter, element) => {
+        let key = iteratee(element);
 
-    array.forEach(element => {
-        let key = iteratee(element)
-        if (key) {
-            key = key.toString();
-        } else {
-            key = undefined;
-        }
+        key = key !== undefined && key !== null ? key.toString() : 'undefined';
 
-        if (counter.hasOwnProperty(key)) {
-            counter[key]++;
-        } else {
-            counter[key] = 1;
-        }
-    })
+        counter[key] = (counter[key] || 0) + 1;
 
-    return counter;
+        return counter;
+    }, {})
 }
