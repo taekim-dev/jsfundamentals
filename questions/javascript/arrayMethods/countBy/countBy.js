aaefec0c6e335752910f33b9666b6833e4d52294
Tae Kim <taekim.dev@gmail.com>
2024-05-28 08:04:00 +0800

    add standard test cases

--- questions/javascript/arrayMethods/countBy/countBy.js
+++ questions/javascript/arrayMethods/countBy/countBy.js
@@ -5,4 +5,4 @@
  */
 export default function countBy(array, iteratee) {
     throw 'Not implemented!';
-  }
+}
--- questions/javascript/arrayMethods/countBy/countBy.test.js
+++ questions/javascript/arrayMethods/countBy/countBy.test.js
@@ -1,21 +1,33 @@
-const methodName = require('./template.js')
+const countBy = require('./countBy.js')
 
 describe('Function Name', () => {
     describe('standard behavior', () => {
-        test('description', () => {
-            expect().toEqual();
+        test('handles an array input', () => {
+            const input = [6.2, 4.3, 6.9];
+            const res = countBy(input, Math.floor); 
+            expect(res['6']).toEqual(2);
+            expect(res['4']).toEqual(1);
+            expect(res['5']).toEqual(undefined);
+        });
+
+        test('handles an object input', () => {
+            const input = [{ n : 3, n : 5, n : 3}]
+            const res = countBy(input, (o) => o.n); 
+
+            expect(res['3']).toEqual(2);
+            expect(res['5']).toEqual(1);
         });
     })
 
     describe('edge cases', () => {
-        test('description', () => {
+        test('handles an empty input', () => {
+            
+        });
+
+        test('handles an undefined input', () => {
         });
     })
 })
-
-
-
-
 
 
 countBy([6.1, 4.2, 6.3], Math.floor);
