40258dbd3fb4070fd854d306759cbe031a5d0ad5
Tae Kim <taekim.dev@gmail.com>
2024-05-29 08:27:25 +0800

    add complex object

--- questions/javascript/randomPractices/forLoop.js
+++ questions/javascript/randomPractices/forLoop.js
@@ -1,5 +1,6 @@
 // comparing forEach vs for...of
-const array = new Array(10000).fill(Math.random());
+// const array = new Array(10000).fill(Math.random());
+const array = Array.from({ length: 10000 }, () => Math.random());
 
 console.time('forEach');
 const res1 = [];
@@ -18,3 +19,23 @@ for (const element of array) {
     }
 }
 console.timeEnd('for...of')
+
+// This time for...of is faster to handle complex object
+const array2 = Array.from({ length: 10000 }, (_, i) => ({ id: i, value: Math.random() }));
+
+console.time('forEach');
+let sum1 = 0;
+array2.forEach(element => {
+    sum1 += element.value;
+});
+console.timeEnd('forEach');
+
+console.time('for...of');
+let sum2 = 0;
+for (const element of array2) {
+    sum2 += element.value;
+}
+console.timeEnd('for...of');
+
+console.log('Sum using forEach:', sum1);
+console.log('Sum using for...of:', sum2);
