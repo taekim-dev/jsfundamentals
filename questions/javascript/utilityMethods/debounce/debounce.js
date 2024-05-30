f28f60a4dffac967334ec3183686d5caf1c20b1d
Tae Kim <taekim.dev@gmail.com>
2024-05-30 08:17:38 +0800

    handle leading option

--- questions/javascript/utilityMethods/debounce/debounce.js
+++ questions/javascript/utilityMethods/debounce/debounce.js
@@ -1,26 +1,37 @@
 /**
  * @param {(...args: any[]) => any} func
  * @param {number} wait
- * @param {boolean} option.leading
- * @param {boolean} option.trailing
+ * @param {Object} [option]
+ * @param {boolean} [option.leading=false]
+ * @param {boolean} [option.trailing=true]
  * @returns {(...args: any[]) => any}
  */
-function debounce(func, wait, option = {leading: false, trailing: true}) {
+function debounce(func, wait, option = { leading: false, trailing: true }) {
     let timer;
-    let leading = option.leading;
+    let lastCallTime = 0;
+    let leadingCalled = false;
+    const { leading, trailing } = option;
+
     function debounced(...args) {
-        if (leading) {
+        const now = Date.now();
+
+        if (leading && now - lastCallTime >= wait) {
             func.apply(this, args);
-            leading = false;
-            
-        } else {
-            clearTimeout(timer);
-            timer = setTimeout(() => {
-                func.apply(this, args);
-            }, wait)
+            leadingCalled = true;
+            lastCallTime = now;
         }
+
+        clearTimeout(timer);
+
+        timer = setTimeout(() => {
+            if (trailing && !leadingCalled) {
+                func.apply(this, args);
+            }
+            leadingCalled = false;
+        }, wait);
     }
+
     return debounced;
 }
-  
+
 module.exports = debounce;
