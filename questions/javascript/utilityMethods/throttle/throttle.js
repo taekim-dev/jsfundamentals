8e5cdc9b40075c791ecc5a95ce565f95ae514b45
Tae Kim <taekim.dev@gmail.com>
2024-05-31 08:52:47 +0800

    add new paramters to throttle

--- questions/javascript/utilityMethods/throttle/throttle.js
+++ questions/javascript/utilityMethods/throttle/throttle.js
@@ -1,15 +1,24 @@
-function throttle(Func, delay) {
-    let timerId = null;
+/**
+ * @param {(...args: any[]) => any} func
+ * @param {number} wait
+ * @param {boolean} option.leading
+ * @param {boolean} option.trailing
+ * @returns {(...args: any[]) => any}
+ */
+function throttle(func, wait, option = {leading: true, trailing: true}) {
+    let timer = null;
 
-    return function(...args) {
-        if(timerId === null) {
-            Func.apply(this, args);
-            timerId = setTimeout(() => {
-                timerId = null;
-            }, delay)
+    function throttled(...args){
+        if (timer === null) {
+            func.apply(this, args);
+            timer = setTimeout(() => {
+                timer = null;
+            }, wait)
         }
     }
+    return throttled;
 }
 
 module.exports = throttle;
+
 
