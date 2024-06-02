878c1dcad3e03a99ed6827a3f38fa8e876bf6912
Tae Kim <taekim.dev@gmail.com>
2024-06-03 07:50:18 +0800

    add skeleton for two way binding

---
+++ questions/dom/binding/model.js
@@ -0,0 +1,21 @@
+/**
+ * @param {{value: string}} state
+ * @param {HTMLInputElement} element
+ */
+function model(state, element) {
+
+}
+
+
+
+
+const input = document.createElement('input')
+const state = { value: 'BFE' }
+model(state, input)
+
+console.log(input.value) // 'BFE'
+state.value = 'dev'
+console.log(input.value) // 'dev'
+input.value = 'BFE.dev'
+input.dispatchEvent(new Event('change'))
+console.log(state.value) // 'BFE.dev'
