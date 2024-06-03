71b2d04f4815b330369d604d6276670dc1b81a77
Tae Kim <taekim.dev@gmail.com>
2024-06-04 06:12:40 +0800

    add a test cases for a single promise

--- questions/javascript/promiseMethod/race/race.test.js
+++ questions/javascript/promiseMethod/race/race.test.js
@@ -1,15 +1,36 @@
-const methodName = require('./template.js')
-// require('./methodName.js')
+const race = require('./race.js')
 
-describe('Function Name', () => {
+describe('Promise Race Test', () => {
     describe('standard behavior', () => {
-        test('description', () => {
+        test('handles a single promise', async () => {
+            const input = [];
+            const promise = new Promise((resolve, reject) => {
+                resolve('successful');
+            })
+            input.push(promise);
+            const res = race(input);
+
+            const output = await res;
+            expect(output).toEqual('successful');
+            
+        });
+
+        test('handles multiple promises with first resolved promise', () => {
+
+
+            expect().toEqual();
+        });
+
+        test('handles multiple promises with first rejected promise', () => {
+
+
             expect().toEqual();
         });
     })
 
     describe('edge cases', () => {
-        test('description', () => {
+        test('handles empty input', () => {
+
         });
     })
 })
