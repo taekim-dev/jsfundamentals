63f35b95760111a5201df7dc967887030a70d498
Tae Kim <taekim.dev@gmail.com>
2024-06-05 06:59:42 +0800

    add more test cases

--- questions/javascript/utilityMethods/once/once.js
+++ questions/javascript/utilityMethods/once/once.js
@@ -12,8 +12,8 @@ function once(func) {
             console.log(res);
         } else {
             console.log(res);
-            return res;
         }
+        return res;
     }
 }
 
@@ -26,3 +26,21 @@ const onced = once(func)
 
 onced(1) // 1, func called with 1
 onced(2) // 1, even 2 is passed, previous result is returned 
+
+function sum(a, b) {
+    return a + b;
+}
+
+const onceSum = once(sum);
+
+console.log(onceSum(1, 2)); // Logs: 3, returns: 3
+console.log(onceSum(3, 4)); // Logs: 3, returns: 3
+
+function greet() {
+    return "Hello!";
+}
+
+const onceGreet = once(greet);
+
+console.log(onceGreet()); // Logs: "Hello!", returns: "Hello!"
+console.log(onceGreet()); // Logs: "Hello!", returns: "Hello!"
