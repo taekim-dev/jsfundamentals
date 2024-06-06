32578c66bb9b6ac23cf8f3bab3ee22fd06bc6d90
Tae Kim <taekim.dev@gmail.com>
2024-06-07 01:16:41 +0800

    add a skeleton for DOM store

---
+++ questions/dom/domStore/domStore.js
@@ -0,0 +1,24 @@
+class NodeStore {
+    /**
+    * @param {Node} node
+    * @param {any} value
+    */
+   set(node, value) {
+    
+   }
+   /**
+    * @param {Node} node
+    * @return {any}
+    */
+   get(node) {
+    
+   }
+   
+   /**
+    * @param {Node} node
+    * @return {Boolean}
+    */
+   has(node) {
+     
+   }
+}
