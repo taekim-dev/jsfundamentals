075cd1c56b4439a8ecd2310247cd27b356dc2efd
Tae Kim <taekim.dev@gmail.com>
2024-06-07 08:31:39 +0800

    improve the function

--- questions/dom/domStore/domStore.js
+++ questions/dom/domStore/domStore.js
@@ -1,31 +1,50 @@
 class NodeStore {
-
-    constructor (){ 
+    constructor() {
         this.nodes = {};
+        this.nodeIdCounter = 0;
     }
+
     /**
-    * @param {Node} node
-    * @param {any} value
-    */
-   set(node, value) {
-       const key = JSON.stringify(node); 
-       this.nodes[key] = value;
-   }
-   /**
-    * @param {Node} node
-    * @return {any}
-    */
-   get(node) {
-      const key = JSON.stringify(node);
-      return this.nodes.hasOwnProperty(key) ? this.nodes[key] : undefined;
-   }
-   
-   /**
-    * @param {Node} node
-    * @return {Boolean}
-    */
-   has(node) {
-    const key = JSON.stringify(node);
-    return this.nodes.hasOwnProperty(key)
-   }
+     * Generates a unique ID for a node and attaches it to the node
+     * @param {Node} node
+     * @return {string} The unique ID for the node
+     */
+    _getNodeId(node) {
+        if (!node.__nodeId) {
+            Object.defineProperty(node, '__nodeId', {
+                value: `node-${this.nodeIdCounter++}`,
+            });
+        }
+        return node.__nodeId;
+    }
+
+    /**
+     * Associates a value with a node
+     * @param {Node} node
+     * @param {any} value
+     */
+    set(node, value) {
+        const key = this._getNodeId(node);
+        this.nodes[key] = value;
+    }
+
+    /**
+     * Retrieves the value associated with a node
+     * @param {Node} node
+     * @return {any}
+     */
+    get(node) {
+        const key = this._getNodeId(node);
+        return this.nodes.hasOwnProperty(key) ? this.nodes[key] : undefined;
+    }
+
+    /**
+     * Checks if a value is associated with a node
+     * @param {Node} node
+     * @return {Boolean}
+     */
+    has(node) {
+        const key = this._getNodeId(node);
+        return this.nodes.hasOwnProperty(key);
+    }
 }
