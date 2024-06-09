fdd6389849423340771d6d89edf99a9438001817
Tae Kim <taekim.dev@gmail.com>
2024-06-09 10:09:33 +0800

    add skeleton for removeChars

---
+++ questions/javascript/randomPractices/removeChars/removeChars.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} input
+ * @returns string
+ */
+function removeChars(input) {
+
+}
+  
+
+removeChars('ab') // 'a'
+removeChars('abc') // ''
+removeChars('cabbaabcca') // 'caa'
+
+console.log(removeChars('ab') === 'a')
+console.log(removeChars('abc') === '')
+console.log(removeChars('cabbaabcca') === 'caa')
