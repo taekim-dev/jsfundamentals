86d0822cdd9aba0d03fd57ba0f3921b98e1f9f9d
Tae Kim <taekim.dev@gmail.com>
2024-06-09 10:16:25 +0800

    add impelmentation of removeChars

--- questions/javascript/randomPractices/removeChars/removeChars.js
+++ questions/javascript/randomPractices/removeChars/removeChars.js
@@ -3,8 +3,32 @@
  * @returns string
  */
 function removeChars(input) {
+    let newStr = [];
+    for(let i = 0; i < input.length; i++) {
+        if (input[i] === 'b'){
+            continue;
+        }
+        newStr.push(input[i]);
+    }
 
+    let j = 0; 
+    let newStr2 = [];
+    while (j < newStr.length) {
+        if (newStr[j] === 'a' && (j+1) < newStr.length) {
+            if (newStr[j+1] === 'c'){
+                j += 2;
+                continue;
+            }
+        }
+        newStr2.push(newStr[j]);
+        j++;
+    }
+
+    console.log(newStr2);
+    return newStr2.toString();
 }
+
+
   
 
 removeChars('ab') // 'a'
