a77bef52dcf068a6f88b5f90933fc0c01bb00262
Tae Kim <taekim.dev@gmail.com>
2024-06-09 09:48:17 +0800

    add implementation

--- questions/javascript/utilityMethods/parallel/parallel.js
+++ questions/javascript/utilityMethods/parallel/parallel.js
@@ -13,5 +13,52 @@ type AsyncFunc = (
  * @return {(callback: Callback) => void}
  */
 function parallel(funcs){
-    
+    return function(callback, data) {
+        let results = [];
+        let completed = 0;
+        let hasError = false;
+
+        funcs.forEach((func, index) => {
+            func((error, result) => {
+                if(hasError) return;
+
+                if(error) {
+                    hasError = true;
+                    callback(error, null);
+                } else {
+                    result[index] = result;
+                    completed += 1;
+
+                    if(completed === func.length) {
+                        callback(null, results);
+                    }
+                }
+            }, data);
+        })
+    }
 }
+
+
+const async1 = (callback) => {
+    callback(undefined, 1)
+}
+
+const async2 = (callback) => {
+    callback(undefined, 2)
+}
+
+const async3 = (callback) => {
+    callback(undefined, 3)
+}
+
+const all = parallel(
+    [
+        async1,
+        async2,
+        async3
+    ]
+)
+
+all((error, data) => {
+    console.log(data) // [1, 2, 3]
+}, 1)
