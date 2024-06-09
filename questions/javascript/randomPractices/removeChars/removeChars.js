5567dcfb9b7f513568f50d3152f6da6e64b5a282
Tae Kim <taekim.dev@gmail.com>
2024-06-09 10:27:27 +0800

    use regular expression

--- questions/javascript/randomPractices/removeChars/removeChars.js
+++ questions/javascript/randomPractices/removeChars/removeChars.js
@@ -3,29 +3,11 @@
  * @returns string
  */
 function removeChars(input) {
-    let newStr = [];
-    for(let i = 0; i < input.length; i++) {
-        if (input[i] === 'b'){
-            continue;
-        }
-        newStr.push(input[i]);
-    }
+    let result = input.replace(/b/g, '');
 
-    let j = 0; 
-    let newStr2 = [];
-    while (j < newStr.length) {
-        if (newStr[j] === 'a' && (j+1) < newStr.length) {
-            if (newStr[j+1] === 'c'){
-                j += 2;
-                continue;
-            }
-        }
-        newStr2.push(newStr[j]);
-        j++;
-    }
+    result = result.replace(/ac/g, '');
 
-    console.log(newStr2);
-    return newStr2.toString();
+    return result;
 }
 
 
@@ -38,3 +20,6 @@ removeChars('cabbaabcca') // 'caa'
 console.log(removeChars('ab') === 'a')
 console.log(removeChars('abc') === '')
 console.log(removeChars('cabbaabcca') === 'caa')
+console.log(removeChars('acbabac') === 'a');
+console.log(removeChars('bacbacbacbac') === '');
+console.log(removeChars('cccbbaac') === 'ccca');
