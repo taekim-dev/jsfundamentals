496e3ab3a0d08f08688ebaf98c447124b443a098
Tae Kim <taekim.dev@gmail.com>
2024-06-09 10:03:02 +0800

    add implementation of decode

--- questions/javascript/randomPractices/decodeMessage/decode.js
+++ questions/javascript/randomPractices/decodeMessage/decode.js
@@ -3,7 +3,33 @@
  * @return {string}
  */
 function decode(message) {
+    if (message.length === 0 || message[0]?.length === 0) return "";
 
+    let row = 0;
+    let col = 0;
+    let decoded = "";
+    let down = true;
+
+    while (col < message[0].length) {
+        decoded += message[row][col];
+
+        if (down) {
+            row += 1;
+            if (row === message.length - 1){
+                down = false;
+            }
+
+        } else {
+            row -= 1;
+            if (row === 0){
+                down = true;
+            }
+        }
+        col += 1;
+
+    }
+
+    return decoded;
 }
 
 const array1 = [
@@ -24,7 +50,7 @@ const array2 = [
 const res2 = decode(array2);
 console.log(res2 === "MUCW")
 
-const array3 = [];
+const array3 = [[]];
 
 const res3 = decode(array3);
 console.log(res3 === "")
