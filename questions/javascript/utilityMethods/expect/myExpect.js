13077efd93ed844382b0e2ef30d5a14bade9b037
Tae Kim <taekim.dev@gmail.com>
2024-06-10 08:16:04 +0800

    update test cases

--- questions/javascript/utilityMethods/expect/myExpect.js
+++ questions/javascript/utilityMethods/expect/myExpect.js
@@ -12,8 +12,30 @@ function myExpect(input) {
 }
   
   
-  
-expect(3).toBe(3) // ✅
-expect(4).toBe(3) // ❌
-expect(3).not.toBe(3) // ❌
-expect(4).not.toBe(3) // ✅
+try {
+    myExpect(3).toBe(3);
+    console.log('test1 passed');
+} catch (e) {
+    console.log('test1 failed');
+}
+
+try {
+    myExpect(4).toBe(3);
+    console.log('test2 failed');
+} catch (e) {
+    console.log('test2 passed');
+}
+
+try {
+    myExpect(3).not.toBe(3);
+    console.log('test3 failed');
+} catch (e) {
+    console.log('test3 passed');
+}
+
+try {
+    myExpect(4).not.toBe(3);
+    console.log('test4 passed');
+} catch (e) {
+    console.log('test4 failed');
+}
