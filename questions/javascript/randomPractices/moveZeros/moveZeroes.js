6df449d22dbe36aa08dba72d9fd28e5d44a7c19d
Tae Kim <taekim.dev@gmail.com>
2024-06-12 07:31:32 +0800

    add a skeleton for move zeros

---
+++ questions/javascript/randomPractices/moveZeros/moveZeroes.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Array<any>} list
+ * @returns {void}
+ */
+function moveZeros(list) {
+    // your code here
+
+}
+  
+
+
+const list = [1,0,0,2,3]
+moveZeros(list) 
+console.log(list) // [1,2,3,0,0]
