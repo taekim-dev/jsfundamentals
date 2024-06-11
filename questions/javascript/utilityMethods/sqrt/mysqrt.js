4d102e14c2784008888931faf500dac927c32fc3
Tae Kim <taekim.dev@gmail.com>
2024-06-11 21:55:52 +0800

    add a skeleton and the test case for mySqrt

---
+++ questions/javascript/utilityMethods/sqrt/mysqrt.js
@@ -0,0 +1,13 @@
+/**
+ * @param {any} x
+ * @return {number}
+ */
+function mySqrt(x) {
+
+}
+
+console.log(mySqrt(0)=== 0)
+console.log(mySqrt(1)=== 1)
+console.log(mySqrt(2)=== 1)
+console.log(mySqrt(4)=== 2)
+console.log(mySqrt(NaN)=== NaN)
