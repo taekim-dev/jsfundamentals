74d0874df7b61992ea0458af04f589e0203ab771
Tae Kim <taekim.dev@gmail.com>
2024-06-11 08:08:05 +0800

    add implementation

--- questions/leetcode/102/levelOrder.js
+++ questions/leetcode/102/levelOrder.js
@@ -1,18 +1,40 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
+/*
  * @param {TreeNode} root
  * @return {number[][]}
  */
 var levelOrderTraversal = function(root) {
-    
+    if (!root) return [];
+
+    const res = [];
+    const queue = [[0, root]];
+
+    while (queue.length > 0) {
+        let [level, node] = queue.shift();
+        if (Array.isArray(res[level])) {
+            res[level].push(node.val);
+        } else {
+            res[level] = [node.val];
+        }
+
+        if (node.left) {
+            queue.push([level + 1, node.left]);
+        } 
+
+        if (node.right) {
+            queue.push([level + 1, node.right])
+        }
+
+    }
+
+    return res;
 };
+
+
 
 
 const input = [3, 9, 20, null, null, 15, 7];
