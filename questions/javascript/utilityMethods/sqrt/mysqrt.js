6c9858d42602032c461d12e372ecccbba32d8913
Tae Kim <taekim.dev@gmail.com>
2024-06-11 22:05:33 +0800

    add basic implementation

--- questions/javascript/utilityMethods/sqrt/mysqrt.js
+++ questions/javascript/utilityMethods/sqrt/mysqrt.js
@@ -3,6 +3,14 @@
  * @return {number}
  */
 function mySqrt(x) {
+    if (isNaN(x) || x < 0) return NaN;
+
+    for(let i = 0; i <= x; i++){
+        const curr = (i * i);
+        const next = ((i+1) * (i+1));
+
+        if (curr <= x && x < next) return i;
+    }
 
 }
 
@@ -10,4 +18,6 @@ console.log(mySqrt(0)=== 0)
 console.log(mySqrt(1)=== 1)
 console.log(mySqrt(2)=== 1)
 console.log(mySqrt(4)=== 2)
-console.log(mySqrt(NaN)=== NaN)
+console.log(mySqrt(50000)=== 223)
+console.log(Object.is(mySqrt(NaN), NaN))
+console.log(Object.is(mySqrt(-1), NaN));
