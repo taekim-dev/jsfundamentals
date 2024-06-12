439bf29fffd05c25d8254582780a93e48d84bb5c
Tae Kim <taekim.dev@gmail.com>
2024-06-12 08:57:27 +0800

    add implementation

--- questions/javascript/randomPractices/moveZeros/moveZeroes.js
+++ questions/javascript/randomPractices/moveZeros/moveZeroes.js
@@ -3,8 +3,26 @@
  * @returns {void}
  */
 function moveZeros(list) {
-    // your code here
+    if (list.length < 2) return list;
+    let zeroCount = 0;
+    let i = 0;
 
+    for (let j  = 0; j < list.length; j++) {
+        if (list[j] !== 0) {
+            list[i] = list[j];
+            i++;
+        } else {
+            zeroCount++;
+        }
+    }
+
+    while (zeroCount > 0) {
+        list[i] = 0;
+        i++;
+        zeroCount--;
+    }
+
+    return list;
 }
   
 
@@ -12,3 +30,11 @@ function moveZeros(list) {
 const list = [1,0,0,2,3]
 moveZeros(list) 
 console.log(list) // [1,2,3,0,0]
+
+const list2 = [0,0,0,0,5,4,3,2,1]
+moveZeros(list2) 
+console.log(list2) // [5,4,3,2,1,0,0,0,0]
+
+const list3 = []
+moveZeros(list3) 
+console.log(list3) // []
