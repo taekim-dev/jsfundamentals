61295b8cafe137323f903180725dff7a22bdfd02
Tae Kim <taekim.dev@gmail.com>
2024-06-13 08:50:04 +0800

    add a test case for promise finally

--- questions/javascript/promiseMethod/finally/myFinally.js
+++ questions/javascript/promiseMethod/finally/myFinally.js
@@ -6,3 +6,24 @@
 function myFinally(promise, onFinally) {
     
 }
+
+let promise = new Promise((resolve, reject) => {
+    const success = true;
+
+    if (success) {
+        resolve('Success!');
+    } else {
+        reject('Error!');
+    }
+})
+
+promise
+    .then((value) => {
+        console.log("Then:", value);
+    })
+    .catch((error) => {
+        console.error("Catch:", error);
+    })
+    .finally(() => {
+        console.log("Finally: this always runs")
+    });
