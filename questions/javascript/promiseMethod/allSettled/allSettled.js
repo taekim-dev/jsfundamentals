ded98633a4c4a37406d21ced31ddaebd7cde197b
Tae Kim <taekim.dev@gmail.com>
2024-06-14 07:55:41 +0800

    add a skeleton for allSettled

---
+++ questions/javascript/promiseMethod/allSettled/allSettled.js
@@ -0,0 +1,12 @@
+
+/**
+ * @param {Array<any>} promises - notice that input might contains non-promises
+ * @return {Promise<Array<{status: 'fulfilled', value: any} | {status: 'rejected', reason: any}>>}
+ */
+function allSettled(promises) {
+
+    
+
+}
+  
+  
