b69d1a7d5edc9e5786116840b5af17a712b23acb
Tae Kim <taekim.dev@gmail.com>
2024-06-14 22:13:16 +0800

    add test cases

--- questions/leetcode/213/houseRobber2.js
+++ questions/leetcode/213/houseRobber2.js
@@ -3,5 +3,18 @@ class Solution {
      * @param {number[]} nums
      * @return {number}
      */
-    rob(nums) {}
+    rob(nums) {
+
+    }
 }
+
+
+const solution = new Solution();
+const res1 = solution.rob([3,4,3]);
+console.log(res1 === 4);
+
+const res2 = solution.rob([2,9,8,3,6]);
+console.log(res2 === 15);
+
+const res3 = solution.rob([]);
+console.log(res3 === 0);
