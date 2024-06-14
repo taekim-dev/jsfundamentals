884e6842174366ef9649fe80013da9cd9015bbf9
Tae Kim <taekim.dev@gmail.com>
2024-06-14 22:07:09 +0800

    add more test cases

--- questions/javascript/promiseMethod/allSettled/allSettled.js
+++ questions/javascript/promiseMethod/allSettled/allSettled.js
@@ -21,3 +21,23 @@ let promise3 = new Promise((resolve) => setTimeout(() => resolve('Delayed Fulfil
 allSettled([promise1, promise2, promise3, 'done']).then(results => {
     console.log(results);
 })
+
+allSettled([]).then((results) => {
+    console.log(results); // []
+})
+
+let singleResolved = Promise.resolve('Single fulfilled');
+allSettled([singleResolved]).then((result) => {
+    console.log(result);
+})
+
+let singleRejected = Promise.resolve('Single rejected');
+allSettled([singleRejected]).then((result) => {
+    console.log(result);
+})
+
+let mixedPromise1 = Promise.resolve('Mixed Fulfilled');
+let mixedPromise2 = Promise.reject('Mixed Rejected');
+allSettled([mixedPromise1, mixedPromise2, 100, 'test']).then((results) => {
+    console.log(results);
+});
