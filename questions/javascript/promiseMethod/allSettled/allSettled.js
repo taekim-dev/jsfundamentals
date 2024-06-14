f35bb4937e6662ca43296b85efbf925022e37376
Tae Kim <taekim.dev@gmail.com>
2024-06-14 08:01:09 +0800

    add a test case

--- questions/javascript/promiseMethod/allSettled/allSettled.js
+++ questions/javascript/promiseMethod/allSettled/allSettled.js
@@ -1,12 +1,19 @@
-
 /**
  * @param {Array<any>} promises - notice that input might contains non-promises
  * @return {Promise<Array<{status: 'fulfilled', value: any} | {status: 'rejected', reason: any}>>}
  */
 function allSettled(promises) {
 
-    
+
 
 }
   
   
+
+let promise1 = Promise.resolve('Fulfilled');    
+let promise2 = Promise.reject('Rejected');
+let promise3 = new Promise((resolve) => setTimeout(() => resolve('Delayed Fulfillment'), 1000));
+
+allSettled([promise1, promise2, promise3, 'done']).then(results => {
+    console.log(results);
+})
