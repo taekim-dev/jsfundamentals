91695cd5d8c2d6a387a27c15b29de71e5d5dfafc
Tae Kim <taekim.dev@gmail.com>
2024-06-15 08:50:15 +0800

    add implementation for house robber

--- questions/leetcode/213/houseRobber2.js
+++ questions/leetcode/213/houseRobber2.js
@@ -4,7 +4,27 @@ class Solution {
      * @return {number}
      */
     rob(nums) {
+        if (nums.length === 0) return 0;
+        if (nums.length === 1) return nums[0];
 
+        const robLinear = (arr) => {
+            let n = arr.length;
+            if (n === 0) return 0;
+            if (n === 1) return arr[0];
+
+            let dp = new Array(n).fill(0);
+            dp[0] = arr[0];
+            dp[1] = Math.max(arr[0], arr[1]);
+
+            for(let i = 2; i < n; i++) {
+                dp[i] = Math.max(dp[i-1], dp[i-2] + arr[i]);
+            }
+
+            return dp[n-1];
+        }
+
+        return Math.max(robLinear(nums.slice(1)), robLinear(nums.slice(0, nums.length - 1)));
+        
     }
 }
 
