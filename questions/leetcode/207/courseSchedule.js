26c4f3276390aa54de22b91a13f19b2800ea3268
Taewan Kim <taekim.dev@gmail.com>
2024-07-22 06:22:33 +0900

    construct adjacency list

--- questions/leetcode/207/courseSchedule.js
+++ questions/leetcode/207/courseSchedule.js
@@ -4,7 +4,29 @@
  * @return {boolean}
  */
 var canFinish = function(numCourses, prerequisites) {
+    const graph = {};
+    for (let i = 0; i < prerequisites.length; i++) {
+        const curr = prerequisites[i][1];
+        const next = prerequisites[i][0];
+        if (curr in graph) {
+            graph[curr].push(next);
+        } else {
+            graph[curr] = [next];
+        }
+    }
+
+    for (let i = 0; i < numCourses; i++) {
+        if (!(i in graph)) {
+            graph[i] = [];
+        }
+    }
+
+    let currMax = 0;
     
+    //bfs or dfs
+
+    return numCourses === currMax;
+
 };
 
 const numCourses1 = 2;
@@ -12,13 +34,17 @@ const prerequisites1 = [[0, 1]];
 console.log(canFinish(numCourses1, prerequisites1) === true)
 
 const numCourses2 = 2;
-const prerequisites2 = [[0, 1], [1,0]];
+const prerequisites2 = [[0, 1], [1, 0]];
 console.log(canFinish(numCourses2, prerequisites2) === false)
 
 const numCourses3 = 5;
 const prerequisites3 = [[0, 1], [2, 0], [3, 2], [4, 1]];
 console.log(canFinish(numCourses3, prerequisites3) === true)
 
-const numCourses4 = 5;
-const prerequisites4 = [[0, 1], [4, 0], [3, 2], [4, 1]];
-console.log(canFinish(numCourses4, prerequisites4) === false)
+const numCourses4 = 3;
+const prerequisites4 = [[0, 1], [1, 2], [2, 0]];
+console.log(canFinish(numCourses4, prerequisites4) === false);
+
+const numCourses5 = 3;
+const prerequisites5 = [];
+console.log(canFinish(numCourses5, prerequisites5) === true);
