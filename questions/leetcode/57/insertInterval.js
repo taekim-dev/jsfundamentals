16ab358aac141c02d597bbe37de782cf572f8357
Taewan Kim <taekim.dev@gmail.com>
2024-07-24 05:27:18 +0900

    add additional test cases

--- questions/leetcode/57/insertInterval.js
+++ questions/leetcode/57/insertInterval.js
@@ -10,8 +10,17 @@ var insert = function(intervals, newInterval) {
 let intervals = [[1,3], [6,9]];
 let newInterval = [2,5];
 let newIntervals = insert(intervals, newInterval);
-console.log(arraysEqual(intervals, newIntervals))
+console.log(arraysEqual(newIntervals, [[1,5], [6,9]]))
 
+let intervals2 = [];
+let newInterval2 = [2,5];
+let newIntervals2 = insert(intervals2, newInterval2);
+console.log(arraysEqual(newInterval2, [[2,5]]))
+
+let intervals3 = [[1,3], [6,9]];
+let newInterval3 = [4,5];
+let newIntervals3 = insert(intervals3, newInterval3);
+console.log(arraysEqual(newIntervals3, [[1,3], [4,5], [6,9]]))
 
 
 var arraysEqual = function(array1, array2) {
