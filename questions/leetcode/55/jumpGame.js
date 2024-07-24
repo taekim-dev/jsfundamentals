7d0be25ae1094f9986fdfcfb5b8ab4f9d26bd803
Taewan Kim <taekim.dev@gmail.com>
2024-07-25 08:17:41 +0900

    add jump game problem and test cases

---
+++ questions/leetcode/55/jumpGame.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    
+};
+
+const nums1 = [2,3,1,1,4]
+console.log(canJump(nums1) === true)
+
+const nums2 = [3,2,1,0,4]
+console.log(canJump(nums2) === false)
+
+const nums3 = [10]
+console.log(canJump(nums3) === true)
+
+const nums4 = [5,0,0,0,0,1]
+console.log(canJump(nums3) === true)
+
