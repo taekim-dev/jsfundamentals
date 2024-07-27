e21ab5f2aeab74ee4c7651ab8614d9398d640d5d
Taewan Kim <taekim.dev@gmail.com>
2024-07-28 08:54:31 +0900

    add valid bst problem

---
+++ questions/leetcode/98/validBST.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    isValidBST(root) {}
+}
