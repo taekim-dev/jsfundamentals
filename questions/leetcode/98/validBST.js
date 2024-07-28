1c56b569094c76548717e2c148322f3459c0738c
Taewan Kim <taekim.dev@gmail.com>
2024-07-28 09:14:01 +0900

    add test cases

--- questions/leetcode/98/validBST.js
+++ questions/leetcode/98/validBST.js
@@ -16,3 +16,21 @@ class Solution {
      */
     isValidBST(root) {}
 }
+
+
+const root1 = null;
+console.log(new Solution().isValidBST(root1) === true);
+
+const root2 = new TreeNode(10);
+console.log(new Solution().isValidBST(root2) === true);
+
+const root3 = new TreeNode(10);
+root3.left = new TreeNode(5);
+root3.right = new TreeNode(15);
+console.log(new Solution().isValidBST(root3) === true);
+
+const root4 = new TreeNode(10);
+root4.left = new TreeNode(15);
+root4.right = new TreeNode(20);
+console.log(new Solution().isValidBST(root4) === false);
+
