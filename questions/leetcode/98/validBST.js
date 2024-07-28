415464d0ce2e2809ffcfabd8de062fde3029c80c
Taewan Kim <taekim.dev@gmail.com>
2024-07-28 09:30:10 +0900

    add solution

--- questions/leetcode/98/validBST.js
+++ questions/leetcode/98/validBST.js
@@ -1,20 +1,27 @@
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     constructor(val = 0, left = null, right = null) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+class TreeNode {
+    constructor(val = 0, left = null, right = null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
 
 class Solution {
     /**
      * @param {TreeNode} root
      * @return {boolean}
      */
-    isValidBST(root) {}
+    isValidBST(root) {
+        return this.isBST(root, -Infinity, Infinity);
+    }
+
+    isBST(node, min, max) {
+        if(!node) return true;
+
+        if(node.val <= min || node.val >= max) return false;
+
+        return this.isBST(node.left, min, node.val) && this.isBST(node.right, node.val, max);
+    }
 }
 
 
@@ -33,4 +40,3 @@ const root4 = new TreeNode(10);
 root4.left = new TreeNode(15);
 root4.right = new TreeNode(20);
 console.log(new Solution().isValidBST(root4) === false);
-
