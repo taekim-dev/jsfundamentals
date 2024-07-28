def09a50aec4cc10864c4900caf01ee78321b803
Taewan Kim <taekim.dev@gmail.com>
2024-07-29 01:54:16 +0800

    add implementation

--- questions/leetcode/49/groupAnagrams.js
+++ questions/leetcode/49/groupAnagrams.js
@@ -6,11 +6,44 @@ class Solution {
      * @return {string[][]}
      */
     groupAnagrams(strs) {
-        // To be implemented
+        
+        const status = new Array(strs.length).fill(false);
+        const res = [];
+
+        for(let i = 0; i < strs.length; i++) {
+            if (status[i]) continue;
+
+            let current = [strs[i]];
+            status[i] = true;
+
+            for (let j = i + 1; j < strs.length; j++) {
+                if (this.isAnagram(strs[i], strs[j])) {
+                    current.push(strs[j]);
+                    status[j] = true;
+                }
+            }
+
+            res.push(current);
+        }
+
+        return res;
+
+    }
+
+    isAnagram(str1, str2) {
+        if (str1.length !== str2.length) return false;
+
+        str1 = str1.split('').sort().join('');
+        str2 = str2.split('').sort().join('');
+
+        for (let i = 0; i < str1.length; i++) {
+            if (str1[i] !== str2[i]) return false;
+        }
+
+        return true;
     }
 }
 
-// Test cases
 const solution = new Solution();
 
 const strs1 = ["act", "pots", "tops", "cat", "stop", "hat"];
