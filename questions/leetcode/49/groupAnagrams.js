209f61934415bfcb644994e29e869141a492f04b
Taewan Kim <taekim.dev@gmail.com>
2024-07-30 06:49:32 +0800

    add anagram equal method

--- questions/leetcode/49/groupAnagrams.js
+++ questions/leetcode/49/groupAnagrams.js
@@ -25,7 +25,7 @@ const solution = new Solution();
 
 const strs1 = ["act", "pots", "tops", "cat", "stop", "hat"];
 const output1 = [["hat"], ["act", "cat"], ["stop", "pots", "tops"]];
-console.log(_.isEqual(solution.groupAnagrams(strs1), output1));
+console.log(areAnagramGroupsEqual(solution.groupAnagrams(strs1), output1));
 
 const strs2 = ["x"];
 const output2 = [["x"]];
@@ -34,3 +34,14 @@ console.log(_.isEqual(solution.groupAnagrams(strs2), output2));
 const strs3 = [""];
 const output3 = [[""]];
 console.log(_.isEqual(solution.groupAnagrams(strs3), output3));
+
+function areAnagramGroupsEqual(actual, expected) {
+    if (actual.length !== expected.length) return false;
+
+    const normalize = (arr) => arr.map(group => group.sort()).sort();
+
+    actual = normalize(actual);
+    expected = normalize(expected);
+
+    return _.isEqual(actual, expected);
+}
