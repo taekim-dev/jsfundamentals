6dd50cb4728a9c5592c525b9147e51704e9e214c
Taewan Kim <taekim.dev@gmail.com>
2024-07-31 07:55:05 +0800

    add dp implementation

--- questions/leetcode/91/decodeWays.js
+++ questions/leetcode/91/decodeWays.js
@@ -2,8 +2,29 @@
  * @param {string} s
  * @return {number}
  */
+
 var numDecodings = function(s) {
+    if (s.length === 0 || s[0] === '0') return 0;
+    if (s.length === 1) return 1;
+
+    const dp = new Array(s.length + 1).fill(0);
+    dp[0] = 1; // empty string
+    dp[1] = s[0] !== '0' ? 1 : 0;
+
+    for (let i = 2; i <= s.length; i++) {
+        const singleDigit = parseInt(s[i-1]);
+        const doubleDigit = parseInt(s.substring(i-2, i));
+
+        if (singleDigit >= 1 && singleDigit <= 9) {
+            dp[i] += dp[i-1];
+        }
+
+        if (doubleDigit >= 10 && doubleDigit <= 26) {
+            dp[i] += dp[i-2];
+        }
+    }
     
+    return dp[s.length];
 };
 
 const input1 = "12";
