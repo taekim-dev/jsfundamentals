c9ba47d98d00b4e4d2293180dff9c5c6a108c0a8
Taewan Kim <taekim.dev@gmail.com>
2024-07-31 06:43:13 +0800

    add decod ways question and test cases

---
+++ questions/leetcode/91/decodeWays.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numDecodings = function(s) {
+    
+};
+
+const input1 = "12";
+console.log(numDecodings(input1) === 2);
+
+const input2 = "226";
+console.log(numDecodings(input2) === 3);
+
+const input3 = "06";
+console.log(numDecodings(input3) === 0);
+
+const input4 = "11106";
+console.log(numDecodings(input4) === 2);
