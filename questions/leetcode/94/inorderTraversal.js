b6d3efbe18558c5ca3914d8c4d1d5c9e854ef2e1
Taewan Kim <taekim.dev@gmail.com>
2024-08-01 08:00:04 +0800

    add a recursive solution

--- questions/leetcode/94/inorderTraversal.js
+++ questions/leetcode/94/inorderTraversal.js
@@ -11,5 +11,24 @@
  * @return {number[]}
  */
 var inorderTraversal = function(root) {
-    
+    let res = [];
+    if (!root) return res;
+
+    res = res.concat(inorderTraversal(root.left));
+    res.push(root.val);
+    res = res.concat(inorderTraversal(root.right));
+
+    return res;
 };
+
+let root1 = new TreeNode(1);
+root1.right = new TreeNode(2);
+root1.right.left = new TreeNode(3);
+//[1, 3, 2]
+
+
+let root2 = null;
+//[]
+
+let root3 = new TreeNode(1);
+//[1]
