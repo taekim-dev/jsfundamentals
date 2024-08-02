c9394486e16add46d1d52929e0cec398f5675152
Taewan Kim <taekim.dev@gmail.com>
2024-08-03 07:26:22 +0800

    add a solution

--- questions/leetcode/11/containerWithMostWater.js
+++ questions/leetcode/11/containerWithMostWater.js
@@ -3,14 +3,31 @@
  * @return {number}
  */
 var maxArea = function(height) {
-    
+    let maxContainer = 0;
+    let left = 0;
+    let right = height.length-1;
 
+    while (left < right) {
+        const leftHeight = height[left];
+        const rightHeight = height[right];
+
+        const currContainer = Math.min(leftHeight, rightHeight) * (right - left);
+        maxContainer = Math.max(currContainer, maxContainer);
+
+        if (left < right) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return maxContainer;
 };
 
 const height1 = [1,8,6,2,5,4,8,3,7];
 const res1 = maxArea(height1);
-console.log(res === 49);
+console.log(res1 === 49);
 
 const height2 = [1,1];
 const res2 = maxArea(height2);
-console.log(res === 1);
+console.log(res2 === 1);
