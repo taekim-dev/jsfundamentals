f9ab399c3f4cdeff75964607ac56fbd594027ae6
Taewan Kim <taekim.dev@gmail.com>
2024-08-03 07:19:35 +0800

    add max water problem and test cases

---
+++ questions/leetcode/11/containerWithMostWater.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    
+
+};
+
+const height1 = [1,8,6,2,5,4,8,3,7];
+const res1 = maxArea(height1);
+console.log(res === 49);
+
+const height2 = [1,1];
+const res2 = maxArea(height2);
+console.log(res === 1);
