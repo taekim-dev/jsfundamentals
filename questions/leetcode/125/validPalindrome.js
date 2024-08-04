b160af74beed59794cbfc747e81e3542662fca0e
Taewan Kim <taekim.dev@gmail.com>
2024-08-05 07:29:55 +0800

    add solution

--- questions/leetcode/125/validPalindrome.js
+++ questions/leetcode/125/validPalindrome.js
@@ -3,7 +3,20 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    
+    s = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+    let i = 0;
+    let j = s.length-1;
+
+    while (i < j) {
+        if (s[i] !== s[j]) {
+            return false;
+        }
+        i++;
+        j--;
+    }
+
+    return true;
 };
 
 
