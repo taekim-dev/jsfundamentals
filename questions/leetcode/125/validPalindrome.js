309fe7ff075cf161e6f81aadb1a513b1691a9dda
Taewan Kim <taekim.dev@gmail.com>
2024-08-05 07:26:07 +0800

    add valid palindrome and test cases

---
+++ questions/leetcode/125/validPalindrome.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    
+};
+
+
+
+const s1 = "A man, a plan, a canal: Panama"
+console.log(isPalindrome(s1) === true)
+
+const s2 = "race a car"
+console.log(isPalindrome(s2) === false)
+
+const s3 = " "
+console.log(isPalindrome(s3) === true)
